e0c373c807bbcfa2cbe6dc669f5f08ee67da8a8d
MrDave1999 <daveseva2010@hotmail.es>
2022-07-11 20:26:56 -0500

    Added GeneralTreatments property in AppDbContext

--- src/DataAccess/AppDbContext.cs
+++ src/DataAccess/AppDbContext.cs
@@ -2,6 +2,8 @@ namespace DentallApp.DataAccess;
 
 public class AppDbContext : DbContext
 {
+    public DbSet<GeneralTreatment> GeneralTreatments { get; set; }
+    
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
         
