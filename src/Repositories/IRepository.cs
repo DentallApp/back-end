002bf27159971be85a2e98c1a43193aa6ce86080
MrDave1999 <daveseva2010@hotmail.es>
2022-07-23 22:37:53 -0500

    Some methods have been renamed
    Additionally the SaveAsync method was added and some methods have been marked as virtual.

--- src/Repositories/IRepository.cs
+++ src/Repositories/IRepository.cs
@@ -4,8 +4,8 @@ public interface IRepository<TEntity> where TEntity : ModelBase
 {
     Task<TEntity> GetByIdAsync(int id);
     Task<IEnumerable<TEntity>> GetAllAsync();
-    void Add(TEntity entity);
+    void Insert(TEntity entity);
     void Update(TEntity entity);
-    void ChangeStateToInactive(TEntity entity);
-    void Remove(TEntity entity);
+    void Delete(TEntity entity);
+    Task<int> SaveAsync();
 }
--- src/Repositories/Repository.cs
+++ src/Repositories/Repository.cs
@@ -2,38 +2,37 @@
 
 public class Repository<TEntity> : IRepository<TEntity> where TEntity : ModelBase
 {
+    private readonly AppDbContext _context;
     private readonly DbSet<TEntity> _entities;
 
     public Repository(AppDbContext context)
     {
+        _context = context;
         _entities = context.Set<TEntity>();
     }
 
-    public async Task<IEnumerable<TEntity>> GetAllAsync()
-        => await _entities.Where(entity => entity.State).ToListAsync();
+    public virtual async Task<IEnumerable<TEntity>> GetAllAsync()
+        => await _entities.ToListAsync();
 
-    public async Task<TEntity> GetByIdAsync(int id)
-        => await _entities.Where(entity => entity.State && entity.Id == id).FirstOrDefaultAsync();
+    public virtual async Task<TEntity> GetByIdAsync(int id)
+        => await _entities.Where(entity => entity.Id == id).FirstOrDefaultAsync();
 
-    public void Add(TEntity entity)
+    public virtual void Insert(TEntity entity)
     {
         entity.CreatedAt = DateTime.UtcNow;
         entity.UpdatedAt = entity.CreatedAt;
         _entities.Add(entity);
     }
 
-    public void Update(TEntity entity)
+    public virtual void Update(TEntity entity)
     {
         entity.UpdatedAt = DateTime.UtcNow;
         _entities.Update(entity);
     }
 
-    public void ChangeStateToInactive(TEntity entity)
-    {
-        entity.State = false;
-        Update(entity);
-    }
-
-    public void Remove(TEntity entity)
+    public virtual void Delete(TEntity entity)
         => _entities.Remove(entity);
+
+    public virtual Task<int> SaveAsync()
+        => _context.SaveChangesAsync();
 }
