9c6dd76910f9a5a578366a6c9e5ff17955db2bcb
MrDave1999 <daveseva2010@hotmail.es>
2022-07-23 22:20:31 -0500

    Added new configuration properties in AppSettings.cs

--- src/Configuration/AppSettings.cs
+++ src/Configuration/AppSettings.cs
@@ -2,6 +2,33 @@ namespace DentallApp.Configuration;
 
 public class AppSettings
 {
+    [EnvKey("BASE_URL")]
+    public string BaseUrl { get; set; }
+
     [EnvKey("CONNECTION_STRING")]
     public string ConnectionString { get; set; }
+
+    [EnvKey("ACCESS_TOKEN_KEY")]
+    public string AccessTokenKey { get; set; }
+
+    [EnvKey("ACCESS_TOKEN_EXPIRES")]
+    public double AccessTokenExpires { get; set; }
+
+    [EnvKey("EMAIL_VERIFICATION_TOKEN_KEY")]
+    public string EmailVerificationTokenKey { get; set; }
+
+    [EnvKey("EMAIL_VERIFICATION_TOKEN_EXPIRES")]
+    public double EmailVerificationTokenExpires { get; set; }
+
+    [EnvKey("REFRESH_TOKEN_EXPIRES")]
+    public double RefreshTokenExpires { get; set; }
+
+    [EnvKey("SENDGRID_API_KEY")]
+    public string SendGridApiKey { get; set; }
+
+    [EnvKey("SENDGRID_FROM_EMAIL")]
+    public string SendGridFromEmail { get; set; }
+
+    [EnvKey("SENDGRID_FROM_NAME")]
+    public string SendGridFromName { get; set; }
 }
