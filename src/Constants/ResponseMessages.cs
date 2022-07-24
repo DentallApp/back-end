ccd86e73ea08595eb4feccf82a98e283ee5deb5b
MrDave1999 <daveseva2010@hotmail.es>
2022-07-23 22:21:59 -0500

    Added server response messages

---
+++ src/Constants/ResponseMessages.cs
@@ -0,0 +1,24 @@
+﻿namespace DentallApp.Constants;
+
+public class ResponseMessages
+{
+    public const string GetResourceMessage = "Recurso obtenido con éxito.";
+    public const string ResourceNotFoundMessage = "Recurso no encontrado.";
+
+    public const string EmailSuccessfullyVerifiedMessage = "Correo electrónico verificado con éxito.";
+    public const string SuccessfulLoginMessage = "Ha iniciado la sesión con éxito.";
+    public const string CouldNotSendEmailMessage = "No se ha podido enviar el correo electrónico.";
+    public const string EmailOrPasswordIncorrectMessage = "El correo electrónico o la contraseña son incorrectos.";
+    public const string EmailNotConfirmedMessage = "El correo electrónico no está confirmado.";
+    public const string CreateBasicUserAccountMessage = "La cuenta de usuario básico se ha creado con éxito. Por favor, confirme su dirección de correo electrónico.";
+    public const string UsernameAlreadyExistsMessage = "El correo electrónico que ingresó está siendo usado por otro usuario.";
+    public const string AccountAlreadyVerifiedMessage = "La cuenta de usuario ya fue verificada.";
+    public const string UsernameNotFoundMessage = "Usuario no encontrado.";
+    public const string AccessTokenInvalidMessage = "El token de acceso es inválido.";
+    public const string EmailVerificationTokenInvalidMessage = "El token de verificación email es inválido.";
+    public const string RefreshTokenInvalidMessage = "Refresh-token es inválido.";
+    public const string RefreshTokenExpiredMessage = "Refresh-token ha expirado.";
+    public const string UpdatedAccessTokenMessage = "Access-token se ha actualizado con éxito.";
+    public const string RevokeTokenMessage = "Refresh token ha sido revocado.";
+    public const string HasNoRefreshTokenMessage = "No tienes un refresh-token para revocar.";
+}
