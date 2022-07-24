4351cbf6c0bac3784c50a76753283c3ef72291c7
MrDave1999 <daveseva2010@hotmail.es>
2022-07-23 22:24:08 -0500

    Added new global usings

--- src/GlobalUsings.cs
+++ src/GlobalUsings.cs
@@ -1,18 +1,47 @@
 global using DentallApp.Features.Chatbot;
+global using DentallApp.Features.StatusManager;
 global using DentallApp.Features.GeneralTreatments;
+global using DentallApp.Features.GeneralTreatments.DTOs;
+global using DentallApp.Features.Persons;
+global using DentallApp.Features.Genders;
+global using DentallApp.Features.Genders.DTOs;
+global using DentallApp.Features.Users;
+global using DentallApp.Features.Users.DTOs;
+global using DentallApp.Features.UserRegistration;
+global using DentallApp.Features.Roles;
+global using DentallApp.Features.UserRoles;
+global using DentallApp.Features.UserRoles.DTOs;
+global using DentallApp.Features.SecurityToken;
+global using DentallApp.Features.SecurityToken.DTOs;
+global using DentallApp.Features.Authentication;
+global using DentallApp.Features.Authentication.DTOs;
+global using DentallApp.Features.EmailSending;
+global using DentallApp.Features.PasswordHasher;
+global using DentallApp.Features.EmailVerification;
 
 global using DentallApp.Configuration;
 global using DentallApp.DataAccess;
 global using DentallApp.Models;
+global using DentallApp.UnitOfWork;
 global using DentallApp.Repositories;
+global using DentallApp.Helpers;
+global using Response = DentallApp.Helpers.Response;
+global using DentallApp.Extensions;
+global using DentallApp.Constants;
+global using static DentallApp.Constants.ResponseMessages;
 
 global using System;
+global using System.Text;
 global using System.Collections.Generic;
 global using System.Threading;
 global using System.Threading.Tasks;
 global using System.IO;
 global using System.Linq;
 global using System.Reflection;
+global using System.Security.Claims;
+global using System.Security.Cryptography;
+global using System.ComponentModel.DataAnnotations.Schema;
+global using System.IdentityModel.Tokens.Jwt;
 
 global using Microsoft.Bot.Builder;
 global using Microsoft.Bot.Builder.Integration.AspNet.Core;
@@ -32,11 +61,20 @@ global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.AspNetCore.Builder;
 global using Microsoft.AspNetCore.Hosting;
 global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Authentication.JwtBearer;
+global using Microsoft.AspNetCore.Authorization;
 
+
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using Microsoft.IdentityModel.Tokens;
 global using Microsoft.OpenApi.Models;
 
 global using Newtonsoft.Json.Linq;
 global using Newtonsoft.Json;
 
 global using DotEnv.Core;
-global using Microsoft.EntityFrameworkCore;
+
+global using SendGrid;
+global using SendGrid.Helpers.Mail;
+global using SendGrid.Extensions.DependencyInjection;
