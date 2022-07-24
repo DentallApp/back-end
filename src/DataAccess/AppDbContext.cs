4203213aef22d1ea06c599292c9a32e1a217a78c
MrDave1999 <daveseva2010@hotmail.es>
2022-07-23 22:36:24 -0500

    Added new configurations and properties DbSet

--- src/DataAccess/AppDbContext.cs
+++ src/DataAccess/AppDbContext.cs
@@ -3,9 +3,23 @@ namespace DentallApp.DataAccess;
 public class AppDbContext : DbContext
 {
     public DbSet<GeneralTreatment> GeneralTreatments { get; set; }
-    
+    public DbSet<Status> Status { get; set; }
+    public DbSet<Person> Persons { get; set; }
+    public DbSet<Gender> Genders { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserRole> UserRoles { get; set; }
+    public DbSet<Role> Roles { get; set; }
+
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
         
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new StatusConfiguration());
+        modelBuilder.ApplyConfiguration(new GeneralTreatmentConfiguration());
+        modelBuilder.ApplyConfiguration(new RoleConfiguration());
+        modelBuilder.ApplyConfiguration(new GenderConfiguration());
     }
 }
