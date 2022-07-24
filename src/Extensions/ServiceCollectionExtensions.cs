fc83347ad96f69cc8a0a2be65dd2c1fcb96c8fcf
MrDave1999 <daveseva2010@hotmail.es>
2022-07-23 22:23:09 -0500

    Added new services and repositories for DI container

---
+++ src/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace DentallApp.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddServices(this IServiceCollection services)
+    {
+        services.AddTransient<IAuthService, AuthService>()
+                .AddTransient<IPasswordHasher, PasswordHasherBcrypt>()
+                .AddTransient<IUserService, UserService>()
+                .AddTransient<IUserRegisterService, UserRegisterService>()
+                .AddTransient<IEmailVerificationService, EmailVerificationService>()
+                .AddTransient<IEmailTemplateService, EmailTemplateService>()
+                .AddTransient<IEmailService, EmailService>()
+                .AddTransient<IGeneralTreatmentService, GeneralTreatmentService>()
+                .AddTransient<ITokenService, TokenService>();
+    }
+
+    public static void AddRepositories(this IServiceCollection services)
+    {
+        services.AddTransient<IUnitOfWork, UnitOfWorkEFCore>();
+        services.AddTransient<IUserRepository, UserRepository>()
+                .AddTransient<IGenderRepository, GenderRepository>()
+                .AddTransient<IGeneralTreatmentRepository, GeneralTreatmentRepository>();    
+    }
+}
