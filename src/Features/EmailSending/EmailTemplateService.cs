aea5c9814587c125ba875f9c70db658bcb0729be
MrDave1999 <daveseva2010@hotmail.es>
2022-07-26 17:27:15 -0500

    EmailConfirmationStyles.html file has been deleted
    The template body and head have been unified into a single file.

--- src/Features/EmailSending/EmailTemplateService.cs
+++ src/Features/EmailSending/EmailTemplateService.cs
@@ -5,9 +5,7 @@ public class EmailTemplateService : IEmailTemplateService
     public async Task<string> LoadTemplateForEmailVerification(string url, string recipientName)
     {
         var template = await File.ReadAllTextAsync("./Templates/EmailConfirmation.html");
-        var templateBody = string.Format(template, recipientName, url);
-        var templateHead = await File.ReadAllTextAsync("./Templates/EmailConfirmationStyles.html");
-        return templateHead + templateBody;
+        return string.Format(template, recipientName, url);
     }
 
     public async Task<string> LoadTemplateForResetPassword(string url, string recipientName)
