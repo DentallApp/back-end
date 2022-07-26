b1ad1c2b1084c04970bc99bdcb5feab6860ede7f
MrDave1999 <daveseva2010@hotmail.es>
2022-07-26 16:05:43 -0500

    Allows authentication from the swagger page

---
+++ src/Extensions/SwaggerGen.cs
@@ -0,0 +1,39 @@
+﻿namespace DentallApp.Extensions;
+
+public static class SwaggerGen
+{
+    public static IServiceCollection AddSwagger(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo { Title = "DentallApi", Version = "v1" });
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Type = SecuritySchemeType.Http,
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Scheme = "bearer",
+                Description = "Please insert JWT token into field"
+            });
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[] { }
+                }
+            });
+            var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+            options.IncludeXmlComments(xmlPath);
+        });
+
+        return services;
+    }
+}
--- src/Startup.cs
+++ src/Startup.cs
@@ -32,13 +32,7 @@ public class Startup
         });
 
         services.AddSendGrid(options => options.ApiKey = settings.SendGridApiKey);
-        services.AddSwaggerGen(options =>
-        {
-            options.SwaggerDoc("v1", new OpenApiInfo { Title = "DentallApi", Version = "v1" });
-            var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-            var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-            options.IncludeXmlComments(xmlPath);
-        });
+        services.AddSwagger();
 
         services.AddAuthentication(options =>
         {
