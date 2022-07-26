8916faabe13ef2a323bf172cd1628b59691c305f
Guillermo Rivera <Riverareyes_2611@hotmail.com>
2022-07-26 05:57:23 -0500

    Union of EmailConfirmation and EmailConfirmationStyles templates

--- src/Features/EmailSending/EmailTemplateService.cs
+++ src/Features/EmailSending/EmailTemplateService.cs
@@ -5,6 +5,8 @@ public class EmailTemplateService : IEmailTemplateService
     public async Task<string> LoadTemplateForEmailVerification(string url, string recipientName)
     {
         var template = await File.ReadAllTextAsync("./Templates/EmailConfirmation.html");
-        return string.Format(template, recipientName, url);
+        var templateBody = string.Format(template, recipientName, url);
+        var templateHead = await File.ReadAllTextAsync("./Templates/EmailConfirmationStyles.html");
+        return templateHead + templateBody;
     }
 }
