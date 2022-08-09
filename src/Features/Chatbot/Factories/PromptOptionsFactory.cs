69c2b330e6abef76cf523cfdd1b6d75bb6c2af20
MrDave1999 <daveseva2010@hotmail.es>
2022-08-09 15:21:14 -0500

    Added two new factory classes

---
+++ src/Features/Chatbot/Factories/PromptOptionsFactory.cs
@@ -0,0 +1,17 @@
+﻿namespace DentallApp.Features.Chatbot.Factories;
+using Attachment = Microsoft.Bot.Schema.Attachment;
+
+public class PromptOptionsFactory
+{
+    public static PromptOptions Create(Attachment attachment, string errorMessage)
+        => new()
+        {
+            Prompt = new Activity
+            {
+                Attachments = new List<Attachment>() { attachment },
+                Type = ActivityTypes.Message,
+
+            },
+            RetryPrompt = MessageFactory.Text(errorMessage)
+        };
+}
