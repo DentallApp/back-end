ce55a5062e13d3f8800935a30dbf8d8493b776f2
MrDave1999 <daveseva2010@hotmail.es>
2022-08-10 23:39:35 -0500

    Added appointment status seeders

--- src/Features/AppoinmentsStatus/AppoinmentStatusConfiguration.cs
+++ src/Features/AppoinmentsStatus/AppoinmentStatusConfiguration.cs
@@ -4,6 +4,49 @@ public class AppoinmentStatusConfiguration : IEntityTypeConfiguration<Appoinment
 {
     public void Configure(EntityTypeBuilder<AppoinmentStatus> builder)
     {
-
+        builder.HasData(
+            new AppoinmentStatus
+            {
+                Id = AppoinmentStatusId.Scheduled,
+                Name = AppoinmentStatusName.Scheduled,
+                CreatedAt = DateTime.UtcNow,
+                UpdatedAt = DateTime.UtcNow
+            },
+            new AppoinmentStatus
+            {
+                Id = 2,
+                Name = "En proceso",
+                CreatedAt = DateTime.UtcNow,
+                UpdatedAt = DateTime.UtcNow
+            },
+            new AppoinmentStatus
+            {
+                Id = 3,
+                Name = "No asistida",
+                CreatedAt = DateTime.UtcNow,
+                UpdatedAt = DateTime.UtcNow
+            },
+            new AppoinmentStatus
+            {
+                Id = 4,
+                Name = "En consulta",
+                CreatedAt = DateTime.UtcNow,
+                UpdatedAt = DateTime.UtcNow
+            },
+            new AppoinmentStatus
+            {
+                Id = 5,
+                Name = "Asistida",
+                CreatedAt = DateTime.UtcNow,
+                UpdatedAt = DateTime.UtcNow
+            },
+            new AppoinmentStatus
+            {
+                Id = 6,
+                Name = "Cancelada",
+                CreatedAt = DateTime.UtcNow,
+                UpdatedAt = DateTime.UtcNow
+            }
+        );
     }
 }
---
+++ src/Features/AppoinmentsStatus/AppoinmentStatusType.cs
@@ -0,0 +1,11 @@
+﻿namespace DentallApp.Features.AppoinmentsStatus;
+
+public class AppoinmentStatusId
+{
+    public const int Scheduled = 1;
+}
+
+public class AppoinmentStatusName
+{
+    public const string Scheduled = "Agendada";
+}
