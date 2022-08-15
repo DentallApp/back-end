c375f5d2e8119a73b9e84306a545255a4c0458bc
MrDave1999 <daveseva2010@hotmail.es>
2022-08-14 23:29:22 -0500

    Add path in dental services

--- src/Features/GeneralTreatments/GeneralTreatmentConfiguration.cs
+++ src/Features/GeneralTreatments/GeneralTreatmentConfiguration.cs
@@ -2,6 +2,9 @@
 
 public class GeneralTreatmentConfiguration : IEntityTypeConfiguration<GeneralTreatment>
 {
+    private string GetPath(string fileName)
+        => Path.Combine(EnvReader.Instance[AppSettings.DentalServicesImagesPath], fileName);
+
     public void Configure(EntityTypeBuilder<GeneralTreatment> builder)
     {
         builder.HasQueryFilterSoftDelete();
@@ -11,7 +14,7 @@ public class GeneralTreatmentConfiguration : IEntityTypeConfiguration<GeneralTre
                  Id = 1,
                  Name = "Ortodoncia/brackets",
                  Description = "Es la ciencia y arte que se encarga de ubicar las piezas dentales.",
-                 ImageUrl = "ortodoncia.png",
+                 ImageUrl = GetPath("ortodoncia.png"),
                  Duration = 40,
                  CreatedAt = DateTime.UtcNow,
                  UpdatedAt = DateTime.UtcNow
@@ -21,7 +24,7 @@ public class GeneralTreatmentConfiguration : IEntityTypeConfiguration<GeneralTre
                  Id = 2,
                  Name = "Calces/resinas",
                  Description = "Su finalidad es la de restaurar o reparar una unidad dentaria (diente) que presenta una cavidad producida por la caries.",
-                 ImageUrl = "calce.png",
+                 ImageUrl = GetPath("calce.png"),
                  Duration = 40,
                  CreatedAt = DateTime.UtcNow,
                  UpdatedAt = DateTime.UtcNow
@@ -31,7 +34,7 @@ public class GeneralTreatmentConfiguration : IEntityTypeConfiguration<GeneralTre
                  Id = 3,
                  Name = "Tratamiento de conductos/endodoncia",
                  Description = "Es un procedimiento que tiene como finalidad preservar las piezas dentales dañadas, evitando así su pérdida.",
-                 ImageUrl = "endodoncia.png",
+                 ImageUrl = GetPath("endodoncia.png"),
                  Duration = 180,
                  CreatedAt = DateTime.UtcNow,
                  UpdatedAt = DateTime.UtcNow
@@ -41,7 +44,7 @@ public class GeneralTreatmentConfiguration : IEntityTypeConfiguration<GeneralTre
                  Id = 4,
                  Name = "Cirugia de tercero molares",
                  Description = "Es el proceso de cirugía oral más frecuentemente derivado por las unidades de salud bucodental de Atención Primaria.",
-                 ImageUrl = "cirugia_molares.png",
+                 ImageUrl = GetPath("cirugia_molares.png"),
                  Duration = 90,
                  CreatedAt = DateTime.UtcNow,
                  UpdatedAt = DateTime.UtcNow
@@ -51,7 +54,7 @@ public class GeneralTreatmentConfiguration : IEntityTypeConfiguration<GeneralTre
                  Id = 5,
                  Name = "Implantes dentales",
                  Description = "Son elementos metálicos que se ubican quirúrgicamente en los huesos maxilares, debajo de las encías.",
-                 ImageUrl = "implantes.png",
+                 ImageUrl = GetPath("implantes.png"),
                  Duration = 180,
                  CreatedAt = DateTime.UtcNow,
                  UpdatedAt = DateTime.UtcNow
@@ -61,7 +64,7 @@ public class GeneralTreatmentConfiguration : IEntityTypeConfiguration<GeneralTre
                  Id = 6,
                  Name = "Diseño de sonrisa",
                  Description = "Es el proceso por el cual se llevan a cabo determinados procesos hasta conseguir el resultado que busca el paciente en lo que a resultados estéticos se refiere.",
-                 ImageUrl = "diseno_sonrisa.png",
+                 ImageUrl = GetPath("diseno_sonrisa.png"),
                  Duration = 40,
                  CreatedAt = DateTime.UtcNow,
                  UpdatedAt = DateTime.UtcNow
@@ -71,7 +74,7 @@ public class GeneralTreatmentConfiguration : IEntityTypeConfiguration<GeneralTre
                  Id = 7,
                  Name = "Blanqueamiento",
                  Description = "Es un tratamiento que se aplica a los dientes que han cambiado de color, siendo uno de los tratamiento estéticos más conservadores.",
-                 ImageUrl = "blanqueamiento.png",
+                 ImageUrl = GetPath("blanqueamiento.png"),
                  Duration = 40,
                  CreatedAt = DateTime.UtcNow,
                  UpdatedAt = DateTime.UtcNow
@@ -81,7 +84,7 @@ public class GeneralTreatmentConfiguration : IEntityTypeConfiguration<GeneralTre
                  Id = 8,
                  Name = "Prótesis fijas/removibles",
                  Description = "Son aquellas en las que sustituimos uno o varios dientes perdidos y se fijan atornilladas o cementadas sobre el implante.",
-                 ImageUrl = "protesis_fijas.png",
+                 ImageUrl = GetPath("protesis_fijas.png"),
                  Duration = 40,
                  CreatedAt = DateTime.UtcNow,
                  UpdatedAt = DateTime.UtcNow
@@ -91,7 +94,7 @@ public class GeneralTreatmentConfiguration : IEntityTypeConfiguration<GeneralTre
                  Id = 9,
                  Name = "Profilaxis/fluorización",
                  Description = "Es una limpieza con técnicas y herramientas que nos permiten eliminar el sarro, como el detartraje, y la placa bacteriana en todas las zonas de la boca.",
-                 ImageUrl = "profilaxis.png",
+                 ImageUrl = GetPath("profilaxis.png"),
                  Duration = 40,
                  CreatedAt = DateTime.UtcNow,
                  UpdatedAt = DateTime.UtcNow
@@ -101,7 +104,7 @@ public class GeneralTreatmentConfiguration : IEntityTypeConfiguration<GeneralTre
                  Id = 10,
                  Name = "Periodoncia",
                  Description = "Es la especialidad de la odontología que trata las enfermedades de las encías y del hueso que sostiene los dientes.",
-                 ImageUrl = "periodoncia.png",
+                 ImageUrl = GetPath("periodoncia.png"),
                  Duration = 40,
                  CreatedAt = DateTime.UtcNow,
                  UpdatedAt = DateTime.UtcNow
@@ -111,7 +114,7 @@ public class GeneralTreatmentConfiguration : IEntityTypeConfiguration<GeneralTre
                  Id = 11,
                  Name = "Odontopediatria",
                  Description = "Es una rama de la Odontología que atiende y trata las distintas enfermedades bucodentales desde la infancia más temprana hasta finalizar el crecimiento.",
-                 ImageUrl = "odontopediatria.png",
+                 ImageUrl = GetPath("odontopediatria.png"),
                  Duration = 40,
                  CreatedAt = DateTime.UtcNow,
                  UpdatedAt = DateTime.UtcNow
