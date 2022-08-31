ec7ab59dcf0928772c00d6a7729a895bf704abf8
MrDave1999 <daveseva2010@hotmail.es>
2022-08-31 18:38:58 -0500

    Added XML comments to a controller

--- src/Features/EmployeeSchedules/EmployeeScheduleController.cs
+++ src/Features/EmployeeSchedules/EmployeeScheduleController.cs
@@ -12,14 +12,24 @@ public class EmployeeScheduleController : ControllerBase
         _employeeScheduleService = employeeScheduleService;
     }
 
+    /// <summary>
+    /// Obtiene todos los horarios de los empleados.
+    /// </summary>
+    /// <returns></returns>
     [HttpGet]
     public async Task<IEnumerable<EmployeeScheduleGetAllDto>> Get()
         => await _employeeScheduleService.GetAllEmployeeSchedulesAsync();
 
+    /// <summary>
+    /// Obtiene el horario de un empleado.
+    /// </summary>
     [HttpGet("{employeeId}")]
     public async Task<IEnumerable<EmployeeScheduleGetDto>> GetByEmployeeId(int employeeId)
         => await _employeeScheduleService.GetEmployeeScheduleByEmployeeIdAsync(employeeId);
 
+    /// <summary>
+    /// Crea un nuevo horario para el empleado.
+    /// </summary>
     [HttpPost]
     public async Task<ActionResult<Response>> Post([FromBody]EmployeeScheduleInsertDto employeeScheduleDto)
     {
@@ -30,6 +40,9 @@ public class EmployeeScheduleController : ControllerBase
         return BadRequest(response);
     }
 
+    /// <summary>
+    /// Actualiza el horario de un empleado.
+    /// </summary>
     [HttpPut("{scheduleId}")]
     public async Task<ActionResult<Response>> Put(int scheduleId, [FromBody]EmployeeScheduleUpdateDto employeeScheduleDto)
     {
