a1bb32c3f0dd7cd1a5532d21f83e36b93a76b6b6
MrDave1999 <daveseva2010@hotmail.es>
2022-09-02 20:56:34 -0500

    Added an interface with its implementation to handle the transaction against the database
    This class is a wrapper to avoid exposing the IDbContextTransaction interface.

---
+++ src/DataAccess/IAppDbContextTransaction.cs
@@ -0,0 +1,9 @@
+﻿namespace DentallApp.DataAccess;
+
+public interface IAppDbContextTransaction : IDisposable, IAsyncDisposable
+{
+    void Commit();
+    Task CommitAsync();
+    void Rollback();
+    Task RollbackAsync();
+}
