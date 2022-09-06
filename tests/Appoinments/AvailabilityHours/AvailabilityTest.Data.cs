c7dd8cf16481da258eb572ad0a61e8957e20f66e
MrDave1999 <daveseva2010@hotmail.es>
2022-09-06 09:37:44 -0500

    Added the unit test for the calculation of available hours

---
+++ tests/Appoinments/AvailabilityHours/AvailabilityTest.Data.cs
@@ -0,0 +1,208 @@
+﻿namespace DentallApp.Tests.Appoinments.AvailabilityHours;
+
+public partial class AvailabilityTest
+{
+    public static IEnumerable<object[]> GetData()
+    {
+        var dentistStartHour = TimeSpan.Parse("7:00");
+        var dentistEndHour   = TimeSpan.Parse("13:10");
+        var unavailables = new List<UnavailableTimeRange>
+        {
+            new() { StartHour = TimeSpan.Parse("9:00"),  EndHour = TimeSpan.Parse("9:30") },
+            new() { StartHour = TimeSpan.Parse("10:00"), EndHour = TimeSpan.Parse("10:30") },
+            new() { StartHour = TimeSpan.Parse("11:00"), EndHour = TimeSpan.Parse("11:40") },
+            new() { StartHour = TimeSpan.Parse("12:40"), EndHour = TimeSpan.Parse("13:10") }
+        };
+
+        yield return new object[]
+        {
+            "test1",
+            new AvailabilityOptions
+            {
+                DentistStartHour = dentistStartHour,
+                DentistEndHour   = dentistEndHour,
+                ServiceDuration  = TimeSpan.FromMinutes(30),
+                Unavailables     = unavailables
+            },
+            new List<AvailableTimeRange>
+            {
+                new() { StartHour = TimeSpan.Parse("7:00"),  EndHour = TimeSpan.Parse("7:30") },
+                new() { StartHour = TimeSpan.Parse("7:30"),  EndHour = TimeSpan.Parse("8:00") },
+                new() { StartHour = TimeSpan.Parse("8:00"),  EndHour = TimeSpan.Parse("8:30") },
+                new() { StartHour = TimeSpan.Parse("8:30"),  EndHour = TimeSpan.Parse("9:00") },
+                new() { StartHour = TimeSpan.Parse("9:30"),  EndHour = TimeSpan.Parse("10:00") },
+                new() { StartHour = TimeSpan.Parse("10:30"), EndHour = TimeSpan.Parse("11:00") },
+                new() { StartHour = TimeSpan.Parse("11:40"), EndHour = TimeSpan.Parse("12:10") },
+                new() { StartHour = TimeSpan.Parse("12:10"), EndHour = TimeSpan.Parse("12:40") }
+            }
+        };
+
+        yield return new object[]
+        {
+            "test2",
+            new AvailabilityOptions
+            {
+                DentistStartHour = dentistStartHour,
+                DentistEndHour   = dentistEndHour,
+                ServiceDuration  = TimeSpan.FromMinutes(40),
+                Unavailables     = unavailables
+            },
+            new List<AvailableTimeRange>
+            {
+                new() { StartHour = TimeSpan.Parse("7:00"),  EndHour = TimeSpan.Parse("7:40") },
+                new() { StartHour = TimeSpan.Parse("7:40"),  EndHour = TimeSpan.Parse("8:20") },
+                new() { StartHour = TimeSpan.Parse("8:20"),  EndHour = TimeSpan.Parse("9:00") },
+                new() { StartHour = TimeSpan.Parse("11:40"), EndHour = TimeSpan.Parse("12:20") }
+            }
+        };
+
+        yield return new object[]
+        {
+            "test3",
+            new AvailabilityOptions
+            {
+                DentistStartHour = dentistStartHour,
+                DentistEndHour   = dentistEndHour,
+                ServiceDuration  = TimeSpan.FromMinutes(60),
+                Unavailables     = unavailables
+            },
+            new List<AvailableTimeRange>
+            {
+                new() { StartHour = TimeSpan.Parse("7:00"), EndHour = TimeSpan.Parse("8:00") },
+                new() { StartHour = TimeSpan.Parse("8:00"), EndHour = TimeSpan.Parse("9:00") },
+                new() { StartHour = TimeSpan.Parse("11:40"), EndHour = TimeSpan.Parse("12:40") }
+            }
+        };
+
+        yield return new object[]
+        {
+            "test4",
+            new AvailabilityOptions
+            {
+                DentistStartHour = dentistStartHour,
+                DentistEndHour   = dentistEndHour,
+                ServiceDuration  = TimeSpan.FromMinutes(90),
+                Unavailables     = unavailables
+            },
+            new List<AvailableTimeRange>
+            {
+                new() { StartHour = TimeSpan.Parse("7:00"), EndHour = TimeSpan.Parse("8:30") }
+            }
+        };
+
+        yield return new object[]
+        {
+            "test5",
+            new AvailabilityOptions
+            {
+                DentistStartHour = dentistStartHour,
+                DentistEndHour   = TimeSpan.Parse("15:00"),
+                ServiceDuration  = TimeSpan.FromMinutes(90),
+                Unavailables     = unavailables
+            },
+            new List<AvailableTimeRange>
+            {
+                new() { StartHour = TimeSpan.Parse("7:00"),  EndHour = TimeSpan.Parse("8:30") },
+                new() { StartHour = TimeSpan.Parse("13:10"), EndHour = TimeSpan.Parse("14:40") }
+            }
+        };
+
+        yield return new object[]
+        {
+            "test6",
+            new AvailabilityOptions
+            {
+                DentistStartHour = TimeSpan.Parse("9:30"),
+                DentistEndHour   = dentistEndHour,
+                ServiceDuration  = TimeSpan.FromMinutes(30),
+                Unavailables     = new List<UnavailableTimeRange>
+                {
+                    new() { StartHour = TimeSpan.Parse("9:00"),  EndHour = TimeSpan.Parse("9:30") },
+                    new() { StartHour = TimeSpan.Parse("12:40"), EndHour = TimeSpan.Parse("13:10") }
+                }
+            },
+            new List<AvailableTimeRange>
+            {
+                new() { StartHour = TimeSpan.Parse("9:30"),   EndHour = TimeSpan.Parse("10:00") },
+                new() { StartHour = TimeSpan.Parse("10:00"),  EndHour = TimeSpan.Parse("10:30") },
+                new() { StartHour = TimeSpan.Parse("10:30"),  EndHour = TimeSpan.Parse("11:00") },
+                new() { StartHour = TimeSpan.Parse("11:00"),  EndHour = TimeSpan.Parse("11:30") },
+                new() { StartHour = TimeSpan.Parse("11:30"),  EndHour = TimeSpan.Parse("12:00") },
+                new() { StartHour = TimeSpan.Parse("12:00"),  EndHour = TimeSpan.Parse("12:30") }
+            }
+        };
+
+        yield return new object[]
+        {
+            "test7",
+            new AvailabilityOptions
+            {
+                DentistStartHour = TimeSpan.Parse("9:30"),
+                DentistEndHour   = TimeSpan.Parse("14:00"),
+                ServiceDuration  = TimeSpan.FromMinutes(30),
+                Unavailables     = new List<UnavailableTimeRange>
+                {
+                    new() { StartHour = TimeSpan.Parse("9:00"),  EndHour = TimeSpan.Parse("9:30") }
+                }
+            },
+            new List<AvailableTimeRange>
+            {
+                new() { StartHour = TimeSpan.Parse("9:30"),   EndHour = TimeSpan.Parse("10:00") },
+                new() { StartHour = TimeSpan.Parse("10:00"),  EndHour = TimeSpan.Parse("10:30") },
+                new() { StartHour = TimeSpan.Parse("10:30"),  EndHour = TimeSpan.Parse("11:00") },
+                new() { StartHour = TimeSpan.Parse("11:00"),  EndHour = TimeSpan.Parse("11:30") },
+                new() { StartHour = TimeSpan.Parse("11:30"),  EndHour = TimeSpan.Parse("12:00") },
+                new() { StartHour = TimeSpan.Parse("12:00"),  EndHour = TimeSpan.Parse("12:30") },
+                new() { StartHour = TimeSpan.Parse("12:30"),  EndHour = TimeSpan.Parse("13:00") },
+                new() { StartHour = TimeSpan.Parse("13:00"),  EndHour = TimeSpan.Parse("13:30") },
+                new() { StartHour = TimeSpan.Parse("13:30"),  EndHour = TimeSpan.Parse("14:00") }
+            }
+        };
+
+        yield return new object[]
+        {
+            "test8",
+            new AvailabilityOptions
+            {
+                DentistStartHour = dentistStartHour,
+                DentistEndHour   = TimeSpan.Parse("11:00"),
+                ServiceDuration  = TimeSpan.FromMinutes(30),
+                Unavailables     = new List<UnavailableTimeRange>
+                {
+                    new() { StartHour = TimeSpan.Parse("9:00"),   EndHour = TimeSpan.Parse("9:30") },
+                    new() { StartHour = TimeSpan.Parse("11:00"),  EndHour = TimeSpan.Parse("11:30") },
+                    new() { StartHour = TimeSpan.Parse("12:40"),  EndHour = TimeSpan.Parse("13:10") }
+                }
+            },
+            new List<AvailableTimeRange>
+            {
+                new() { StartHour = TimeSpan.Parse("7:00"),  EndHour = TimeSpan.Parse("7:30") },
+                new() { StartHour = TimeSpan.Parse("7:30"),  EndHour = TimeSpan.Parse("8:00") },
+                new() { StartHour = TimeSpan.Parse("8:00"),  EndHour = TimeSpan.Parse("8:30") },
+                new() { StartHour = TimeSpan.Parse("8:30"),  EndHour = TimeSpan.Parse("9:00") },
+                new() { StartHour = TimeSpan.Parse("9:30"),  EndHour = TimeSpan.Parse("10:00") },
+                new() { StartHour = TimeSpan.Parse("10:00"), EndHour = TimeSpan.Parse("10:30") },
+                new() { StartHour = TimeSpan.Parse("10:30"), EndHour = TimeSpan.Parse("11:00") }
+            }
+        };
+
+        yield return new object[]
+        {
+            "test9",
+            new AvailabilityOptions
+            {
+                DentistStartHour = TimeSpan.Parse("8:00"),
+                DentistEndHour   = TimeSpan.Parse("12:00"),
+                ServiceDuration  = TimeSpan.FromMinutes(60),
+                Unavailables     = new List<UnavailableTimeRange>()
+            },
+            new List<AvailableTimeRange>
+            {
+                new() { StartHour = TimeSpan.Parse("8:00"),  EndHour = TimeSpan.Parse("9:00") },
+                new() { StartHour = TimeSpan.Parse("9:00"),  EndHour = TimeSpan.Parse("10:00") },
+                new() { StartHour = TimeSpan.Parse("10:00"), EndHour = TimeSpan.Parse("11:00") },
+                new() { StartHour = TimeSpan.Parse("11:00"), EndHour = TimeSpan.Parse("12:00") }
+            }
+        };
+    }
+}
---
+++ tests/Appoinments/AvailabilityHours/AvailabilityTest.cs
@@ -0,0 +1,59 @@
+﻿namespace DentallApp.Tests.Appoinments.AvailabilityHours;
+
+[TestClass]
+public partial class AvailabilityTest
+{
+    [DataTestMethod]
+    [DynamicData(nameof(GetData), DynamicDataSourceType.Method)]
+    public void GetAvailableHours_WhenNumberOfUnavailableHoursIsGreaterThanOrEqualToZero_ShouldReturnAvailableHours(
+        string testId, AvailabilityOptions options, List<AvailableTimeRange> expected)
+    {
+        var availableHours = Availability.GetAvailableHours(options);
+
+        Assert.AreEqual(expected.Count, actual: availableHours.Count);
+        for(int i = 0; i < availableHours.Count; i++)
+        {
+            Assert.AreEqual(expected[i].StartHour, actual: availableHours[i].StartHour);
+            Assert.AreEqual(expected[i].EndHour,   actual: availableHours[i].EndHour);
+        }
+    }
+
+    [TestMethod]
+    public void GetAvailableHours_WhenDurationOfDentalServiceIsEqualToZero_ShouldThrowArgumentException()
+    {
+        var options = new AvailabilityOptions
+        {
+            Unavailables     = new List<UnavailableTimeRange>(),
+            DentistStartHour = TimeSpan.Parse("9:30"),
+            DentistEndHour   = TimeSpan.Parse("12:30"),
+            ServiceDuration  = TimeSpan.FromMinutes(0)
+        };
+
+        void action() => Availability.GetAvailableHours(options);
+
+        Assert.ThrowsException<InvalidOperationException>(action);
+    }
+
+    [TestMethod]
+    public void GetAvailableHours_WhenNumberOfAvailableHoursIsZero_ShouldReturnNull()
+    {
+        var unavailables = new List<UnavailableTimeRange>
+        {
+            new() { StartHour = TimeSpan.Parse("8:00"),  EndHour = TimeSpan.Parse("9:00") },
+            new() { StartHour = TimeSpan.Parse("9:00"),  EndHour = TimeSpan.Parse("10:00") },
+            new() { StartHour = TimeSpan.Parse("10:00"), EndHour = TimeSpan.Parse("11:00") },
+            new() { StartHour = TimeSpan.Parse("11:00"), EndHour = TimeSpan.Parse("12:00") }
+        };
+        var options = new AvailabilityOptions
+        {
+            Unavailables     = unavailables,
+            DentistStartHour = TimeSpan.Parse("8:00"),
+            DentistEndHour   = TimeSpan.Parse("12:00"),
+            ServiceDuration  = TimeSpan.FromMinutes(60)
+        };
+
+        var availableHours = Availability.GetAvailableHours(options);
+
+        Assert.IsNull(availableHours);
+    }
+}
