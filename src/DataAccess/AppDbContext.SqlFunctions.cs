55eefb5d292b1f8996b0dbc41a7a9b680baa7d7f
MrDave1999 <daveseva2010@hotmail.es>
2022-09-07 22:31:25 -0500

    Two MariaDb specific functions have been integrated in EF Core
    EF Core does not support ADDTIME and CONVERT functions, so we had to add them for the EF Core translation.

---
+++ src/DataAccess/AppDbContext.SqlFunctions.cs
@@ -0,0 +1,34 @@
+﻿namespace DentallApp.DataAccess;
+
+public partial class AppDbContext
+{
+    public DateTime AddTime(DateTime dateTime, TimeSpan addTime) 
+        => throw new InvalidOperationException();
+
+    public DateTime ToDateTime(DateTime dateTime) 
+        => throw new InvalidOperationException();
+
+    public void AddSqlFunctions(ModelBuilder modelBuilder)
+    {
+        var addTimeMethodInfo = typeof(AppDbContext) // Your DB Context
+            .GetRuntimeMethod(nameof(AppDbContext.AddTime), new[] { typeof(DateTime), typeof(TimeSpan) });
+
+        modelBuilder.HasDbFunction(() => AddTime(default, default))
+           .HasTranslation(args => new SqlFunctionExpression(
+                   functionName: "ADDTIME",
+                   arguments: new[] { args.ToArray()[0], args.ToArray()[1] },
+                   nullable: false,
+                   argumentsPropagateNullability: new[] { false, false },
+                   type: typeof(DateTime),
+                   typeMapping: null));
+
+        modelBuilder.HasDbFunction(() => ToDateTime(default))
+           .HasTranslation(args => new SqlFunctionExpression(
+                   functionName: "CONVERT",
+                   arguments: new[] { args.ToArray()[0], new SqlFragmentExpression("datetime") },
+                   nullable: false,
+                   argumentsPropagateNullability: new[] { false, false },
+                   type: typeof(DateTime),
+                   typeMapping: null));
+    }
+}
