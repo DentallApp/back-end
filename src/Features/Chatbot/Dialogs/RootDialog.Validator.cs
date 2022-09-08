79a2bca9ff8e564fe2206cda8d4c51e715284c1e
MrDave1999 <daveseva2010@hotmail.es>
2022-09-07 23:30:02 -0500

    Partial classes were removed from the RootDialog class
    The reason why the partial classes were removed was because it is difficult to understand the cascade dialog step by step.

--- src/Features/Chatbot/Dialogs/RootDialog.Validator.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DentallApp.Features.Chatbot.Dialogs;
-
-public partial class RootDialog
-{
-    private async Task<bool> ValidateChoiceSet(PromptValidatorContext<JObject> promptContext, CancellationToken cancellationToken)
-        => await Task.FromResult(promptContext.Context.Activity.Value != null);
-}
--- src/Features/Chatbot/Dialogs/RootDialog.cs
+++ src/Features/Chatbot/Dialogs/RootDialog.cs
@@ -14,10 +14,81 @@ public partial class RootDialog : ComponentDialog
                 ShowNameOfOffices,
                 ShowNameOfServices,
                 ShowNameOfDentists,
+                ShowAppoinmentDate,
                 ShowAppointmentData
         };
 
         AddDialog(new WaterfallDialog(nameof(WaterfallDialog), waterfallSteps));
         AddDialog(new AdaptiveCardPrompt(nameof(AdaptiveCardPrompt), ValidateChoiceSet));
+    }
+
+    private async Task<bool> ValidateChoiceSet(PromptValidatorContext<JObject> promptContext, CancellationToken cancellationToken)
+        => await Task.FromResult(promptContext.Context.Activity.Value is not null);
+
+    private async Task<DialogTurnResult> ShowNameOfPatients(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+    {
+        var userProfile = stepContext.CreateUserProfile();
+        var choicesTask = _botService.GetPatientsAsync(userProfile);
+        var cardJsonTask = TemplateCardLoader.LoadPatientCardAsync();
+        var choices = await choicesTask;
+        var cardJson = await cardJsonTask;
+        return await stepContext.PromptAsync(
+            nameof(AdaptiveCardPrompt),
+            AdaptiveCardFactory.CreateSingleChoiceCard(cardJson, choices),
+            cancellationToken
+        );
+    }
+
+    private async Task<DialogTurnResult> ShowNameOfOffices(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+    {
+        var choicesTask = _botService.GetOfficesAsync();
+        var cardJsonTask = TemplateCardLoader.LoadOfficeCardAsync();
+        var choices = await choicesTask;
+        var cardJson = await cardJsonTask;
+        return await stepContext.PromptAsync(
+            nameof(AdaptiveCardPrompt),
+            AdaptiveCardFactory.CreateSingleChoiceCard(cardJson, choices),
+            cancellationToken
+        );
+    }
+
+    private async Task<DialogTurnResult> ShowNameOfServices(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+    {
+        var value = JObject.Parse(stepContext.Context.Activity.Value.ToString());
+        stepContext.Values["officeId"] = (string)value["officeId"];
+        var choicesTask = _botService.GetDentalServicesAsync();
+        var cardJsonTask = TemplateCardLoader.LoadDentalServiceCardAsync();
+        var choices = await choicesTask;
+        var cardJson = await cardJsonTask;
+        return await stepContext.PromptAsync(
+            nameof(AdaptiveCardPrompt),
+            AdaptiveCardFactory.CreateSingleChoiceCard(cardJson, choices),
+            cancellationToken
+        );
+    }
+
+    private async Task<DialogTurnResult> ShowNameOfDentists(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+    {
+        int officeId = int.Parse(stepContext.Values["officeId"].ToString());
+        var choicesTask = _botService.GetDentistsByOfficeIdAsync(officeId);
+        var cardJsonTask = TemplateCardLoader.LoadDentistCardAsync();
+        var choices = await choicesTask;
+        var cardJson = await cardJsonTask;
+        return await stepContext.PromptAsync(
+            nameof(AdaptiveCardPrompt),
+            AdaptiveCardFactory.CreateSingleChoiceCard(cardJson, choices),
+            cancellationToken
+        );
+    }
+
+    private async Task<DialogTurnResult> ShowAppoinmentDate(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    private async Task<DialogTurnResult> ShowAppointmentData(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+    {
+        await stepContext.Context.SendActivityAsync("Bot en construcción... Lo siento...", cancellationToken: cancellationToken);
+        return await stepContext.EndDialogAsync(cancellationToken: cancellationToken);
     }
 }
