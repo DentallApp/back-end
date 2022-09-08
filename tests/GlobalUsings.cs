ce56d0c74de19bbd84451c90a88dc96beab2990a
MrDave1999 <daveseva2010@hotmail.es>
2022-09-08 14:58:59 -0500

    Added unit test for herocards
    The test checks if the 'CreateSchedulesHeroCard' method includes the available hours in the card actions of each hero card.
    
    In addition, it is verified if the available hours are distributed in different hero cards to form the carousel.

--- tests/GlobalUsings.cs
+++ tests/GlobalUsings.cs
@@ -1,3 +1,5 @@
 global using Microsoft.VisualStudio.TestTools.UnitTesting;
+global using Microsoft.Bot.Schema;
 global using DentallApp.Features.AvailabilityHours;
 global using DentallApp.Features.AvailabilityHours.DTOs;
+global using DentallApp.Features.Chatbot.Factories;
