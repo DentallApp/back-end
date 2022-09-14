cc799c76a8501c71f5140fe058454d94c89f4c83
MrDave1999 <daveseva2010@hotmail.es>
2022-09-14 12:13:26 -0500

    Added a synchronous version of the IInstantMessaging.SendMessage method

--- src/Features/InstantMessaging/WhatsAppMessaging.cs
+++ src/Features/InstantMessaging/WhatsAppMessaging.cs
@@ -13,18 +13,30 @@ public class WhatsAppMessaging : IInstantMessaging
         _settings = settings;
     }
 
-    public async Task<string> SendMessageAsync(string phoneNumber, string message)
+    private CreateMessageOptions ConfigureOptions(string phoneNumber, string message)
     {
         TwilioClient.Init(_settings.TwilioAccountSid, _settings.TwilioAuthToken);
-        var util   = PhoneNumberUtil.GetInstance();
-        var number = util.Parse(phoneNumber, _settings.DefaultRegion);
-        var to     = util.Format(number, PhoneNumberFormat.E164);
+        var util    = PhoneNumberUtil.GetInstance();
+        var number  = util.Parse(phoneNumber, _settings.DefaultRegion);
+        var to      = util.Format(number, PhoneNumberFormat.E164);
         var messageOptions = new CreateMessageOptions(
             new Twilio.Types.PhoneNumber($"whatsapp:{to}"));
         messageOptions.From = new Twilio.Types.PhoneNumber($"whatsapp:{_settings.TwilioFromNumber}");
         messageOptions.Body = message;
+        return messageOptions;
+    }
 
+    public async Task<string> SendMessageAsync(string phoneNumber, string message)
+    {
+        var messageOptions  = ConfigureOptions(phoneNumber, message);
         var messageResource = await MessageResource.CreateAsync(messageOptions);
+        return messageResource.Body;
+    }
+
+    public string SendMessage(string phoneNumber, string message)
+    {
+        var messageOptions  = ConfigureOptions(phoneNumber, message);
+        var messageResource = MessageResource.Create(messageOptions);
         return messageResource.Body;
     }
 }
