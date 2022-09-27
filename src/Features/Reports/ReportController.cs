d2b4eebc4112b3b1b6a0b862b93a7bb0a692291d
MrDave1999 <daveseva2010@hotmail.es>
2022-09-26 22:19:12 -0500

    Added validation so that the admin can only generate reports for the office to which he/she belongs

--- src/Features/Reports/ReportController.cs
+++ src/Features/Reports/ReportController.cs
@@ -16,20 +16,32 @@ public class ReportController : ControllerBase
 	/// Obtiene el reporte sobre el total de citas asistidas, no asistidas y canceladas.
 	/// </summary>
 	[HttpPost("appoinment")]
-	public async Task<ReportGetTotalAppoinmentDto> GetTotalAppoinmentsByDateRange([FromBody]ReportPostWithDentistDto reportPostDto)
-		=> await _reportQuery.GetTotalAppoinmentsByDateRangeAsync(reportPostDto);
+	public async Task<ActionResult<ReportGetTotalAppoinmentDto>> GetTotalAppoinmentsByDateRange([FromBody]ReportPostWithDentistDto reportPostDto)
+	{
+		if (User.IsAdmin() && User.IsNotInOffice(reportPostDto.OfficeId))
+			return Unauthorized();
+		return Ok(await _reportQuery.GetTotalAppoinmentsByDateRangeAsync(reportPostDto));
+	}
 
-    /// <summary>
-    /// Obtiene el reporte sobre el total de citas agendadas por odontólogo.
-    /// </summary>
-    [HttpPost("appoinment/scheduled")]
-    public async Task<IEnumerable<ReportGetTotalScheduledAppoinmentDto>> GetTotalScheduledAppoinmentsByDateRange([FromBody]ReportPostDto reportPostDto)
-        => await _reportQuery.GetTotalScheduledAppoinmentsByDateRangeAsync(reportPostDto);
+	/// <summary>
+	/// Obtiene el reporte sobre el total de citas agendadas por odontólogo.
+	/// </summary>
+	[HttpPost("appoinment/scheduled")]
+	public async Task<ActionResult<IEnumerable<ReportGetTotalScheduledAppoinmentDto>>> GetTotalScheduledAppoinmentsByDateRange([FromBody]ReportPostDto reportPostDto)
+	{
+        if (User.IsAdmin() && User.IsNotInOffice(reportPostDto.OfficeId))
+            return Unauthorized();
+        return Ok(await _reportQuery.GetTotalScheduledAppoinmentsByDateRangeAsync(reportPostDto));
+	}
 
 	/// <summary>
 	/// Obtiene el reporte de los servicios dentales más solicitados.
 	/// </summary>
 	[HttpPost("most-requested/services")]
-	public async Task<IEnumerable<ReportGetMostRequestedServicesDto>> GetMostRequestedServices([FromBody]ReportPostDto reportPostDto)
-		=> await _reportQuery.GetMostRequestedServicesAsync(reportPostDto);
+	public async Task<ActionResult<IEnumerable<ReportGetMostRequestedServicesDto>>> GetMostRequestedServices([FromBody]ReportPostDto reportPostDto)
+	{
+        if (User.IsAdmin() && User.IsNotInOffice(reportPostDto.OfficeId))
+            return Unauthorized();
+        return Ok(await _reportQuery.GetMostRequestedServicesAsync(reportPostDto));
+	}
 }
