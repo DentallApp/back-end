8b8e5249fed1580a5fc150082a7d54e403577a3a
MrDave1999 <daveseva2010@hotmail.es>
2022-12-03 05:39:21 -0500

    Send requests to the database once a day to remind appointments

--- src/Startup.cs
+++ src/Startup.cs
@@ -54,7 +54,7 @@ public class Startup
 
         services.AddAuthorization();
         services.AddBotServices();
-        services.AddHostedService<TimedHostedService>();
+        services.AddQuartzJobs(settings);
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
