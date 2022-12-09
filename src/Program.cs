a9a2e3f1825e6b2c6a3d15aef57ced0e9c46fa5b
MrDave1999 <daveseva2010@hotmail.es>
2022-12-08 21:49:56 -0500

    Added support for top-level statements

--- src/Program.cs
+++ src/Program.cs
@@ -1,16 +1,54 @@
-﻿namespace DentallApp;
+﻿var builder = WebApplication.CreateBuilder(args);
 
-public class Program
+// Add services to the container.
+
+builder.Services.AddServices()
+                .AddRepositories()
+                .AddHelpers();
+
+// Sets environment variables from a .env file.
+new EnvLoader().Load();
+var settings = new EnvBinder().Bind<AppSettings>();
+
+builder.Services.AddSingleton(settings)
+                .AddSingleton<IDbConnector>(new MariaDbConnector(settings.ConnectionString));
+
+builder.Services.AddHttpClient()
+                .AddControllers(options => options.SuppressAsyncSuffixInActionNames = false)
+                .AddCustomInvalidModelStateResponse()
+                .AddNewtonsoftJson();
+
+
+builder.Services.AddDbContext(settings);
+builder.Services.AddSendGrid(options => options.ApiKey = settings.SendGridApiKey);
+builder.Services.AddSwagger();
+builder.Services.AddAuthenticationJwtBearer(settings);
+builder.Services.AddBotServices();
+builder.Services.AddQuartzJobs(settings);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public static void Main(string[] args)
-    {
-        CreateHostBuilder(args).Build().Run();
-    }
-
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(options => options.SwaggerEndpoint("v1/swagger.json", "DentallApi V1"));
 }
+
+app.UseDefaultFiles()
+   .UseStaticFiles()
+   .UseWebSockets()
+   .UsePathBase(new PathString("/api"))
+   .UseRouting()
+   .UseCors(options =>
+    {
+       options.AllowAnyOrigin();
+       options.AllowAnyMethod();
+       options.AllowAnyHeader();
+    })
+   .UseAuthentication()
+   .UseAuthorization()
+   .UseEndpoints(endpoints => endpoints.MapControllers());
+
+app.Run();
