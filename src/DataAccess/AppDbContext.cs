6b32df2b9b9f9025a5855a27b0a6c590b8aff1ea
MrDave1999 <daveseva2010@hotmail.es>
2022-12-12 12:18:15 -0500

    Removed DbSets from the AppDbContext class
    In the code the generic Set<T> method is being used, so these properties are unusable and only saturate the context instance.

--- src/DataAccess/AppDbContext.cs
+++ src/DataAccess/AppDbContext.cs
@@ -2,49 +2,44 @@ namespace DentallApp.DataAccess;
 
 public partial class AppDbContext : CustomDbContext
 {
-    public DbSet<GeneralTreatment> GeneralTreatments { get; set; }
-    public DbSet<SpecificTreatment> SpecificTreatments { get; set; }
-    public DbSet<Person> Persons { get; set; }
-    public DbSet<Gender> Genders { get; set; }
-    public DbSet<User> Users { get; set; }
-    public DbSet<UserRole> UserRoles { get; set; }
-    public DbSet<Role> Roles { get; set; }
-    public DbSet<Dependent> Dependents { get; set; }
-    public DbSet<Kinship> Kinships { get; set; }
-    public DbSet<Employee> Employees { get; set; }
-    public DbSet<Office> Offices { get; set; }
-    public DbSet<Appointment> Appointments { get; set; }
-    public DbSet<AppointmentStatus> AppointmentsStatus { get; set; }
-    public DbSet<EmployeeSchedule> EmployeeSchedules { get; set; }
-    public DbSet<WeekDay> WeekDays { get; set; }
-    public DbSet<FavoriteDentist> FavoriteDentists { get; set; }
-    public DbSet<OfficeSchedule> OfficeSchedules { get; set; }
-
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-    {
-        
-    }
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.AddDelegateDecompiler();
-    }
+        => optionsBuilder.AddDelegateDecompiler();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.AddEntity<GeneralTreatment>()
+                    .AddEntity<SpecificTreatment>()
+                    .AddEntity<Person>()
+                    .AddEntity<Gender>()
+                    .AddEntity<User>()
+                    .AddEntity<UserRole>()
+                    .AddEntity<Role>()
+                    .AddEntity<Dependent>()
+                    .AddEntity<Kinship>()
+                    .AddEntity<Employee>()
+                    .AddEntity<Office>()
+                    .AddEntity<Appointment>()
+                    .AddEntity<AppointmentStatus>()
+                    .AddEntity<EmployeeSchedule>()
+                    .AddEntity<WeekDay>()
+                    .AddEntity<FavoriteDentist>()
+                    .AddEntity<OfficeSchedule>();
+
         AddSqlFunctions(modelBuilder);
 
-        modelBuilder.ApplyConfiguration(new GeneralTreatmentConfiguration());
-        modelBuilder.ApplyConfiguration(new SpecificTreatmentConfiguration());
-        modelBuilder.ApplyConfiguration(new RoleConfiguration());
-        modelBuilder.ApplyConfiguration(new GenderConfiguration());
-        modelBuilder.ApplyConfiguration(new KinshipConfiguration());
-        modelBuilder.ApplyConfiguration(new DependentConfiguration());
-        modelBuilder.ApplyConfiguration(new OfficeConfiguration());
-        modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
-        modelBuilder.ApplyConfiguration(new AppointmentStatusConfiguration());
-        modelBuilder.ApplyConfiguration(new WeekDayConfiguration());
-        modelBuilder.ApplyConfiguration(new EmployeeScheduleConfiguration());
-        modelBuilder.ApplyConfiguration(new OfficeScheduleConfiguration());
+        modelBuilder.ApplyConfiguration(new GeneralTreatmentConfiguration())
+                    .ApplyConfiguration(new SpecificTreatmentConfiguration())
+                    .ApplyConfiguration(new RoleConfiguration())
+                    .ApplyConfiguration(new GenderConfiguration())
+                    .ApplyConfiguration(new KinshipConfiguration())
+                    .ApplyConfiguration(new DependentConfiguration())
+                    .ApplyConfiguration(new OfficeConfiguration())
+                    .ApplyConfiguration(new EmployeeConfiguration())
+                    .ApplyConfiguration(new AppointmentStatusConfiguration())
+                    .ApplyConfiguration(new WeekDayConfiguration())
+                    .ApplyConfiguration(new EmployeeScheduleConfiguration())
+                    .ApplyConfiguration(new OfficeScheduleConfiguration());
     }
 }
