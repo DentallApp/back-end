699c5728a78560e59b78558c678ffe71bb982033
MrDave1999 <daveseva2010@hotmail.es>
2022-12-15 14:28:42 -0500

    Add extensions for SoftDeleteEntity

---
+++ src/Extensions/SoftDeleteEntityExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace DentallApp.Extensions;
+
+public static class SoftDeleteEntityExtensions
+{
+    [Decompile]
+    public static string GetStatusName(this SoftDeleteEntity entity)
+        => entity.IsDeleted ? StatusType.Inactive : StatusType.Active;
+
+    [Decompile]
+    public static bool IsActive(this SoftDeleteEntity entity)
+        => !entity.IsDeleted;
+
+    [Decompile]
+    public static bool IsInactive(this SoftDeleteEntity entity)
+        => entity.IsDeleted;
+}
