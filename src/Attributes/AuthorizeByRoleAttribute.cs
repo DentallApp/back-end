77df974e8eb6a7741d448a6f79a60cd3476f3465
MrDave1999 <daveseva2010@hotmail.es>
2022-12-19 11:01:09 -0500

    Added Attributes layer to provide declarative information

---
+++ src/Attributes/AuthorizeByRoleAttribute.cs
@@ -0,0 +1,9 @@
+﻿namespace DentallApp.Attributes;
+
+public class AuthorizeByRoleAttribute : AuthorizeAttribute
+{
+    public AuthorizeByRoleAttribute(params string[] roles)
+    {
+        Roles = string.Join(",", roles);
+    }
+}
---
+++ src/Attributes/ImageAttribute.cs
@@ -0,0 +1,18 @@
+﻿namespace DentallApp.Attributes;
+
+public class ImageAttribute : ValidationAttribute
+{
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        var formFile = value as IFormFile;
+        if (formFile == null)
+            return ValidationResult.Success;
+
+        var fileStream = formFile.OpenReadStream();
+        bool isRecognizableType = FileTypeValidator.IsTypeRecognizable(fileStream);
+        if (!isRecognizableType)
+            return new ValidationResult(UnrecognizableFileMessage);
+        bool isImage = fileStream.IsImage();
+        return isImage ? ValidationResult.Success : new ValidationResult(NotAnImageMessage);
+    }
+}
