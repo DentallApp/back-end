f4caac1a496677af4763f1d2eda7f01d8d3aaa2b
MrDave1999 <daveseva2010@hotmail.es>
2022-12-19 11:17:27 -0500

    Add a comment before the conditional

--- src/Attributes/ImageAttribute.cs
+++ src/Attributes/ImageAttribute.cs
@@ -5,6 +5,8 @@ public class ImageAttribute : ValidationAttribute
     protected override ValidationResult IsValid(object value, ValidationContext validationContext)
     {
         var formFile = value as IFormFile;
+
+        // This is so that the image can be optional when updating an entity.
         if (formFile is null)
             return ValidationResult.Success;
 
