7760618ec2cb114f685318217f91ded752f21428
MrDave1999 <daveseva2010@hotmail.es>
2022-12-22 12:57:13 -0500

    Use 'UpdateEntities' method to simplify the code

--- src/Features/UserRoles/UserRoleRepository.cs
+++ src/Features/UserRoles/UserRoleRepository.cs
@@ -4,25 +4,7 @@ public class UserRoleRepository : Repository<UserRole>, IUserRoleRepository
 {
     public UserRoleRepository(AppDbContext context) : base(context) { }
 
-    public void UpdateUserRoles(int userId, IOrderedEnumerable<UserRole> currentUserRoles, IOrderedEnumerable<int> newRoles)
-    {
-        if (currentUserRoles.Count() == newRoles.Count())
-        {
-            currentUserRoles.Zip(newRoles, (currentUserRole, newRoleId) =>
-            {
-                currentUserRole.RoleId = newRoleId;
-                return currentUserRole;
-            }).ToList();
-        }
-        else
-        {
-            foreach (UserRole currentUserRole in currentUserRoles)
-                if (!newRoles.Contains(currentUserRole.RoleId))
-                    Delete(currentUserRole);
-
-            foreach (int newRoleId in newRoles)
-                if (!currentUserRoles.Any(currentUserRole => currentUserRole.RoleId == newRoleId))
-                    Insert(new UserRole { UserId = userId, RoleId = newRoleId });
-        }
-    }
+    /// <inheritdoc />
+    public void UpdateUserRoles(int userId, List<UserRole> currentUserRoles, List<int> rolesId)
+        => this.UpdateEntities(key: userId, source: ref currentUserRoles, newEntries: ref rolesId);
 }
