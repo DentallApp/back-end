cfb521f0a972a76e3f7c1af94b7d25ad92e069c5
MrDave1999 <daveseva2010@hotmail.es>
2022-12-23 18:54:53 -0500

    Added method that updates the offices assigned to a public holiday

---
+++ src/Features/PublicHolidays/Offices/HolidayOfficeRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace DentallApp.Features.PublicHolidays.Offices;
+
+public class HolidayOfficeRepository : Repository<HolidayOffice>, IHolidayOfficeRepository
+{
+    public HolidayOfficeRepository(AppDbContext context) : base(context) { }
+
+    /// <inheritdoc />
+    public void UpdateHolidayOffices(int publicHolidayId, List<HolidayOffice> currentHolidayOffices, List<int> officesId)
+        => this.AddOrUpdateOrDelete(key: publicHolidayId, source: ref currentHolidayOffices, identifiers: ref officesId);
+}
