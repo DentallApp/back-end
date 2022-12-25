9201bb2592ad6263a6a9c47bf85d95b36cadc3a8
MrDave1999 <daveseva2010@hotmail.es>
2022-12-24 19:05:22 -0500

    Move two modules from the Features layer to Helpers
    InstantMessaging and PasswordHasher modules are not features, but helpers.

---
+++ src/Helpers/PasswordHasher/IPasswordHasher.cs
@@ -0,0 +1,7 @@
+﻿namespace DentallApp.Helpers.PasswordHasher;
+
+public interface IPasswordHasher
+{
+    bool Verify(string text, string passwordHash);
+    string HashPassword(string text);
+}
---
+++ src/Helpers/PasswordHasher/PasswordHasherBcrypt.cs
@@ -0,0 +1,10 @@
+﻿namespace DentallApp.Helpers.PasswordHasher;
+
+public class PasswordHasherBcrypt : IPasswordHasher
+{
+    public string HashPassword(string text)
+        => BCrypt.Net.BCrypt.HashPassword(text);
+
+    public bool Verify(string text, string passwordHash)
+        => BCrypt.Net.BCrypt.Verify(text, passwordHash);
+}
