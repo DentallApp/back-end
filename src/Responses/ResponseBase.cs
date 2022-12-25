c76c03269633076283a868b947d1141aa1ad8b40
MrDave1999 <daveseva2010@hotmail.es>
2022-12-25 11:25:19 -0500

    Move some classes from the Helpers to the Responses layer
    The classes that represent the HTTP response are not helpers, for that reason it was decided to move the classes to a new layer.

---
+++ src/Responses/ResponseBase.cs
@@ -0,0 +1,18 @@
+﻿namespace DentallApp.Responses;
+
+public class ResponseBase
+{
+    public bool Success { get; set; }
+    public string Message { get; set; }
+    public IDictionary<string, IEnumerable<string>> Errors { get; set; }
+
+    public ResponseBase()
+    {
+
+    }
+
+    public ResponseBase(string message)
+    {
+        Message = message;
+    }
+}
---
+++ src/Responses/ResponseOfT.cs
@@ -0,0 +1,16 @@
+﻿namespace DentallApp.Responses;
+
+public class Response<T> : ResponseBase
+{
+    public T Data { get; set; }
+
+    public Response()
+    {
+        
+    }
+
+    public Response(string message) : base(message)
+    {
+
+    }
+}
