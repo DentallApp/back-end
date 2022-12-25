786ae53937747f089f92cd3266323d59d8f7db0f
MrDave1999 <daveseva2010@hotmail.es>
2022-12-24 19:03:33 -0500

    Removed files

--- src/Features/InstantMessaging/IInstantMessaging.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DentallApp.Features.InstantMessaging;
-
-public interface IInstantMessaging
-{
-    Task<string> SendMessageAsync(string phoneNumber, string message);
-    string SendMessage(string phoneNumber, string message);
-}
