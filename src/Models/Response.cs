139ec6f080a1518d1f403e52b1d467d42030dccb
MrDave1999 <daveseva2010@hotmail.es>
2022-12-24 19:41:38 -0500

    Move some classes from the Helpers to the Models layer
    The ResponseBase, Response and ResponseOfT classes represent data, so they are considered as models.
    
    The difference between models and entities in this context is that entities represent the database schema and models only represent data (such as DTOs) and can have methods if required.

---
+++ src/Models/Response.cs
@@ -0,0 +1,16 @@
+﻿namespace DentallApp.Models;
+
+public class Response : ResponseBase
+{
+    public object Data { get; set; }
+
+    public Response()
+    {
+        
+    }
+
+    public Response(string message) : base(message)
+    {
+
+    }
+}
---
+++ src/Models/ResponseOfT.cs
@@ -0,0 +1,16 @@
+﻿namespace DentallApp.Models;
+
+public class Response<T> : ResponseBase
+{
+    public T Data { get; set; }
+
+    public Response()
+    {
+        
+    }
+
+    public Response(string message) : base(message)
+    {
+
+    }
+}
