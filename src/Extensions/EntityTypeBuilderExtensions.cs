f239c6d2a4dbac5267daac71188af356028060e5
MrDave1999 <daveseva2010@hotmail.es>
2023-01-02 18:56:07 -0500

    Removed 'AddSeedData' from EntityTypeBuilderExtensions

--- src/Extensions/EntityTypeBuilderExtensions.cs
+++ src/Extensions/EntityTypeBuilderExtensions.cs
@@ -4,14 +4,4 @@ public static class EntityTypeBuilderExtensions
 {
     public static EntityTypeBuilder<TEntity> HasQueryFilterSoftDelete<TEntity>(this EntityTypeBuilder<TEntity> builder) where TEntity : SoftDeleteEntity
         => builder.HasQueryFilter(entity => !entity.IsDeleted);
-
-    public static DataBuilder<TEntity> AddSeedData<TEntity>(this EntityTypeBuilder<TEntity> builder, params TEntity[] data) where TEntity : EntityBase
-    {
-        foreach(var entity in data)
-        {
-            entity.CreatedAt = DateTime.Now;
-            entity.UpdatedAt = DateTime.Now;
-        }
-        return builder.HasData(data);
-    }
 }
