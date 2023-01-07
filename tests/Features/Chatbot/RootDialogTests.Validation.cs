8bba0aac236e49dd0907de1366f3760954a6e40c
MrDave1999 <daveseva2010@hotmail.es>
2023-01-07 18:34:10 -0500

    Renamed a local variable to 'incomingActivity'

--- tests/Features/Chatbot/RootDialogTests.Validation.cs
+++ tests/Features/Chatbot/RootDialogTests.Validation.cs
@@ -5,38 +5,38 @@ public partial class RootDialogTests
     [TestMethod]
     public async Task Bot_WhenIncomingActivityHasValuePropertyWithNull_ShouldSendAnErrorMessage()
     {
-        var activity = new Activity { Value = default };
+        var incomingActivity = new Activity { Value = default };
         await _testClient.SendActivityAsync<IMessageActivity>(CreateInitialActivity());
         _testClient.GetNextReply<IMessageActivity>();
-        var reply = await _testClient.SendActivityAsync<IMessageActivity>(activity);
+        var reply = await _testClient.SendActivityAsync<IMessageActivity>(incomingActivity);
         Assert.AreEqual(expected: SelectPatientMessage, actual: reply.Text);
 
         await _testClient.SendActivityAsync<IMessageActivity>(CreateActivityWithSelectedPatientId());
         _testClient.GetNextReply<IMessageActivity>();
-        reply     = await _testClient.SendActivityAsync<IMessageActivity>(activity);
+        reply     = await _testClient.SendActivityAsync<IMessageActivity>(incomingActivity);
         Assert.AreEqual(expected: SelectOfficeMessage, actual: reply.Text);
 
         await _testClient.SendActivityAsync<IMessageActivity>(CreateActivityWithSelectedOfficeId());
         _testClient.GetNextReply<IMessageActivity>();
-        reply = await _testClient.SendActivityAsync<IMessageActivity>(activity);
+        reply = await _testClient.SendActivityAsync<IMessageActivity>(incomingActivity);
         Assert.AreEqual(expected: SelectDentalServiceMessage, actual: reply.Text);
 
         await _testClient.SendActivityAsync<IMessageActivity>(CreateActivityWithSelectedDentalServiceId());
         _testClient.GetNextReply<IMessageActivity>();
-        reply = await _testClient.SendActivityAsync<IMessageActivity>(activity);
+        reply = await _testClient.SendActivityAsync<IMessageActivity>(incomingActivity);
         Assert.AreEqual(expected: SelectDentistMessage, actual: reply.Text);
 
         Environment.SetEnvironmentVariable(AppSettings.MaxDaysInCalendar, "60");
         await _testClient.SendActivityAsync<IMessageActivity>(CreateActivityWithSelectedDentistId());
         _testClient.GetNextReply<IMessageActivity>();
         _testClient.GetNextReply<IMessageActivity>();
-        reply = await _testClient.SendActivityAsync<IMessageActivity>(activity);
+        reply = await _testClient.SendActivityAsync<IMessageActivity>(incomingActivity);
         Assert.AreEqual(expected: SelectAppointmentDateMessage, actual: reply.Text);
 
         await _testClient.SendActivityAsync<IMessageActivity>(CreateActivityWithSelectedDate());
         _testClient.GetNextReply<IMessageActivity>();
         _testClient.GetNextReply<IMessageActivity>();
-        reply = await _testClient.SendActivityAsync<IMessageActivity>(activity);
+        reply = await _testClient.SendActivityAsync<IMessageActivity>(incomingActivity);
         Assert.AreEqual(expected: SelectScheduleMessage, actual: reply.Text);
     }
 
