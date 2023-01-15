df448839b127e94dc29bc30bb350ce19cfc951b2
MrDave1999 <daveseva2010@hotmail.es>
2023-01-14 20:34:07 -0500

    Added XML comments to the private class

--- src/Extensions/AuthenticationJwtBearer.cs
+++ src/Extensions/AuthenticationJwtBearer.cs
@@ -30,13 +30,17 @@ public static class AuthenticationJwtBearer
     private static AuthenticationBuilder AddCustomJwtBearer(this AuthenticationBuilder builder, Action<JwtBearerOptions> configureOptions)
         => builder.AddScheme<JwtBearerOptions, CustomJwtBearerHandler>(JwtBearerDefaults.AuthenticationScheme, displayName: null, configureOptions);
 
+    /// <inheritdoc />
+    /// <remarks>
+    /// This custom class was created so that when DirectLine channel sends a request to the bot, it does not throw an exception:
+    /// <see cref="SecurityTokenSignatureKeyNotFoundException" />.
+    /// <para>See issue <see href="https://github.com/DentallApp/back-end/issues/135">#135</see>.</para>
+    /// </remarks>
     private class CustomJwtBearerHandler : JwtBearerHandler
     {
         public CustomJwtBearerHandler(IOptionsMonitor<JwtBearerOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock)
             : base(options, logger, encoder, clock) { }
 
-        // This class was created so that when DirectLine channel sends a request to the bot, it does not throw an exception:
-        // SecurityTokenSignatureKeyNotFoundException.
         protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
             => Context.Request.Path.StartsWithSegments("/messages") ? 
                 AuthenticateResult.NoResult() : 
