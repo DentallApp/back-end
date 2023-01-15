5111cd8ab5ac1d75c3df0d500a4afa11b77ded1c
Dave Roman <43916038+MrDave1999@users.noreply.github.com>
2023-01-14 20:38:50 -0500

    Fixed signature validation failed (#136)
    * Added XML comments to the private class
    
    * Added constant to represent the bot path

--- src/Extensions/AuthenticationJwtBearer.cs
+++ src/Extensions/AuthenticationJwtBearer.cs
@@ -9,7 +9,7 @@ public static class AuthenticationJwtBearer
             options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
             options.DefaultChallengeScheme    = JwtBearerDefaults.AuthenticationScheme;
         })
-        .AddJwtBearer(options =>
+        .AddCustomJwtBearer(options =>
         {
             options.RequireHttpsMetadata = false;
             options.SaveToken = true;
@@ -25,5 +25,30 @@ public static class AuthenticationJwtBearer
         });
         services.AddAuthorization();
         return services;
+    }
+
+    private static AuthenticationBuilder AddCustomJwtBearer(this AuthenticationBuilder builder, Action<JwtBearerOptions> configureOptions)
+        => builder.AddScheme<JwtBearerOptions, CustomJwtBearerHandler>(JwtBearerDefaults.AuthenticationScheme, displayName: null, configureOptions);
+
+    /// <inheritdoc />
+    /// <remarks>
+    /// This custom class was created so that when DirectLine channel sends a request to the bot, it does not throw an exception:
+    /// <see cref="SecurityTokenSignatureKeyNotFoundException" />.
+    /// <para>See issue <see href="https://github.com/DentallApp/back-end/issues/135">#135</see>.</para>
+    /// </remarks>
+    private class CustomJwtBearerHandler : JwtBearerHandler
+    {
+        /// <summary>
+        /// Path associated to the bot.
+        /// </summary>
+        private const string BotPath = "/messages";
+
+        public CustomJwtBearerHandler(IOptionsMonitor<JwtBearerOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock)
+            : base(options, logger, encoder, clock) { }
+
+        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+            => Context.Request.Path.StartsWithSegments(BotPath) ? 
+                AuthenticateResult.NoResult() : 
+                await base.HandleAuthenticateAsync();
     }
 }
