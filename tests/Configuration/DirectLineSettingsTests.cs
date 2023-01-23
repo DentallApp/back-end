d9e70dfb2bc075141b0b48bbd786fa8066ad49f0
MrDave1999 <daveseva2010@hotmail.es>
2023-01-23 13:22:12 -0500

    Added unit tests for the 'DirectLineSettings' class

---
+++ tests/Configuration/DirectLineSettingsTests.cs
@@ -0,0 +1,78 @@
+﻿namespace DentallApp.Tests.Configuration;
+
+[TestClass]
+public class DirectLineSettingsTests
+{
+    private const string LocalHostBaseUrl = "http://localhost:5000";
+    private IEnvBinder _envBinder;
+
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        _envBinder = new EnvBinder().AllowBindNonPublicProperties();
+        Environment.SetEnvironmentVariable(DirectLineSettings.DirectLineSecretSetting, "SECRET");
+    }
+
+    [TestMethod]
+    public void GetDirectLineBaseUrl_WhenDirectLineBaseUrlIsEmptyOrWhiteSpace_ShouldReturnsDefaultBaseUrl()
+    {
+        Environment.SetEnvironmentVariable(DirectLineSettings.DirectLineBaseUrlSetting, " ");
+        var settings = _envBinder.Bind<DirectLineSettings>();
+
+        var baseUrl = settings.GetDirectLineBaseUrl();
+
+        Assert.AreEqual(expected: DirectLineSettings.DefaultBaseUrl, actual: baseUrl);
+    }
+
+    [DataTestMethod]
+    [DataRow(LocalHostBaseUrl + "/")]
+    [DataRow(LocalHostBaseUrl)]
+    [DataRow(LocalHostBaseUrl + "///")]
+    public void GetDirectLineBaseUrl_WhenDirectLineBaseUrlIsNotEmpty_ShouldReturnsBaseUrlWithSlashAtEnd(string value)
+    {
+        Environment.SetEnvironmentVariable(DirectLineSettings.DirectLineBaseUrlSetting, value);
+        var settings = _envBinder.Bind<DirectLineSettings>();
+
+        var baseUrl = settings.GetDirectLineBaseUrl();
+
+        Assert.AreEqual(expected: "http://localhost:5000/", actual: baseUrl);
+    }
+
+    [TestMethod]
+    public void GetProviderName_WhenBaseUrlIsEmptyOrWhiteSpace_ShouldReturnsDefaultProviderName()
+    {
+        Environment.SetEnvironmentVariable(DirectLineSettings.DirectLineBaseUrlSetting, " ");
+        var settings = _envBinder.Bind<DirectLineSettings>();
+
+        var providerName = settings.GetProviderName();
+
+        Assert.AreEqual(expected: DirectLineSettings.DefaultProviderName, actual: providerName);
+    }
+
+    [DataTestMethod]
+    [DataRow(DirectLineSettings.DefaultBaseUrl)]
+    [DataRow(DirectLineSettings.DefaultBaseUrl + "///")]
+    public void GetProviderName_WhenBaseUrlStartsWithDefaultBaseUrl_ShouldReturnsDefaultProviderName(string baseUrl)
+    {
+        Environment.SetEnvironmentVariable(DirectLineSettings.DirectLineBaseUrlSetting, baseUrl);
+        var settings = _envBinder.Bind<DirectLineSettings>();
+
+        var providerName = settings.GetProviderName();
+
+        Assert.AreEqual(expected: DirectLineSettings.DefaultProviderName, actual: providerName);
+    }
+
+    [DataTestMethod]
+    [DataRow(LocalHostBaseUrl + "/")]
+    [DataRow(LocalHostBaseUrl)]
+    [DataRow(LocalHostBaseUrl + "///")]
+    public void GetProviderName_WhenBaseUrlNotStartsWithDefaultBaseUrl_ShouldReturnsInDirectLineService(string baseUrl)
+    {
+        Environment.SetEnvironmentVariable(DirectLineSettings.DirectLineBaseUrlSetting, baseUrl);
+        var settings = _envBinder.Bind<DirectLineSettings>();
+
+        var providerName = settings.GetProviderName();
+
+        Assert.AreEqual(expected: nameof(InDirectLineService), actual: providerName);
+    }
+}
--- tests/GlobalUsings.cs
+++ tests/GlobalUsings.cs
@@ -6,6 +6,7 @@ global using Microsoft.Bot.Builder.Testing;
 global using Telerik.JustMock;
 global using System.Security.Claims;
 global using Newtonsoft.Json.Linq;
+global using DotEnv.Core;
 
 global using DentallApp.Features.AvailabilityHours;
 global using DentallApp.Features.AvailabilityHours.DTOs;
@@ -13,8 +14,6 @@ global using DentallApp.Features.Chatbot.Factories;
 global using DentallApp.Features.WeekDays;
 global using DentallApp.Features.AppointmentCancellation;
 global using DentallApp.Features.AppointmentCancellation.DTOs;
-global using DentallApp.Helpers.InstantMessaging;
-global using DentallApp.Helpers.DateTimeHelpers;
 global using DentallApp.Features.SecurityToken;
 global using DentallApp.Features.Roles;
 global using DentallApp.Features.Appointments;
@@ -23,15 +22,19 @@ global using DentallApp.Features.EmployeeSchedules;
 global using DentallApp.Features.EmployeeSchedules.DTOs;
 global using DentallApp.Features.GeneralTreatments;
 global using DentallApp.Features.GeneralTreatments.DTOs;
+global using DentallApp.Features.SpecificTreatments.DTOs;
 global using DentallApp.Features.PublicHolidays.Offices;
 global using DentallApp.Features.Chatbot;
 global using DentallApp.Features.Chatbot.Dialogs;
 global using DentallApp.Features.Chatbot.Models;
+global using DentallApp.Features.Chatbot.DirectLine.Providers;
+
 global using DentallApp.Entities;
+global using DentallApp.Helpers.InstantMessaging;
+global using DentallApp.Helpers.DateTimeHelpers;
 global using DentallApp.Configuration;
 global using DentallApp.DataAccess.Repositories;
 global using DentallApp.Responses;
-global using DentallApp.Features.SpecificTreatments.DTOs;
 global using static DentallApp.Constants.ResponseMessages;
 
 global using static DentallApp.Tests.Features.Chatbot.BotServiceMockFactory;
