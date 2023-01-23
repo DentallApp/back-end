6d924099edc6c4df09133b39d9128c4393150095
MrDave1999 <daveseva2010@hotmail.es>
2023-01-23 13:03:25 -0500

    Added the class that represents the Direct Line configuration

---
+++ src/Configuration/DirectLineSettings.cs
@@ -0,0 +1,30 @@
+﻿namespace DentallApp.Configuration;
+
+public class DirectLineSettings
+{
+    public const string DirectLineSecretSetting  = "DIRECT_LINE_SECRET";
+    public const string DirectLineBaseUrlSetting = "DIRECT_LINE_BASE_URL";
+    public const string DefaultBaseUrl           = "https://directline.botframework.com/"; 
+    public const string DefaultProviderName      = nameof(DirectLineAzureService);
+
+    public string DirectLineSecret { get; set; }
+    private string DirectLineBaseUrl { get; set; }
+
+    public string GetDirectLineBaseUrl()
+        => string.IsNullOrWhiteSpace(DirectLineBaseUrl) ? 
+                  DefaultBaseUrl : 
+                  DirectLineBaseUrl.TrimEnd('/') + "/";
+
+    public string GetProviderName()
+    {
+        var baseUrl = GetDirectLineBaseUrl();
+        return string.IsNullOrWhiteSpace(baseUrl) ?
+                  DefaultProviderName :
+                  GetServiceName(baseUrl);
+    }
+
+    private static string GetServiceName(string baseUrl)
+        => baseUrl.StartsWith(DefaultBaseUrl) ?
+                  DefaultProviderName :
+                  nameof(InDirectLineService);
+}
