e8fa5d8b7158ced0d8590d9a460d60865c5a981c
MrDave1999 <daveseva2010@hotmail.es>
2023-01-23 13:18:34 -0500

    Added extension method to register Direct Line service

---
+++ src/Features/Chatbot/DirectLine/DirectLineExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace DentallApp.Features.Chatbot.DirectLine;
+
+public static class DirectLineExtensions
+{
+    public static IServiceCollection AddDirectLineService(this IServiceCollection services)
+    {
+        var settings = new EnvBinder()
+                           .AllowBindNonPublicProperties()
+                           .Bind<DirectLineSettings>();
+
+        services.AddSingleton(settings);
+        var providerName = settings.GetProviderName();
+        services.AddHttpClient(
+            name: providerName, 
+            httpClient => httpClient.BaseAddress = new Uri(settings.GetDirectLineBaseUrl())
+        );
+
+        services.AddTransient(
+            serviceType:        typeof(DirectLineService),
+            implementationType: Type.GetType(GetProviderTypeName(providerName))
+        );
+        return services;
+    }
+
+    private static string GetProviderTypeName(string providerName)
+        => $"{typeof(DirectLineService).Namespace}.{providerName}";
+}
