9774041f7812d6242182a5757116fa88802d3200
MrDave1999 <daveseva2010@hotmail.es>
2023-01-23 13:16:36 -0500

    Added a route for generate a Direct Line token

---
+++ src/Features/Chatbot/DirectLine/DirectLineController.cs
@@ -0,0 +1,21 @@
+﻿namespace DentallApp.Features.Chatbot.DirectLine;
+
+[Route("directline/token")]
+[ApiController]
+public class DirectLineController : ControllerBase
+{
+	private readonly DirectLineService _directLineService;
+
+	public DirectLineController(DirectLineService directLineService)
+    {
+        _directLineService = directLineService;
+    }
+
+    [AuthorizeByRole(RolesName.BasicUser)]
+    [HttpGet]
+    public async Task<ActionResult<Response<DirectLineGetTokenDto>>> Get()
+    {
+        var response = await _directLineService.GetTokenAsync(User.GetUserId());
+        return response.Success ? Ok(response) : BadRequest(response);
+    }
+}
