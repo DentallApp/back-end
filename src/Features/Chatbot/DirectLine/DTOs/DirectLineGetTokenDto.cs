cc43152f1f08a1c7752ee2ad1e405de8aa8edc74
MrDave1999 <daveseva2010@hotmail.es>
2023-01-23 13:06:45 -0500

    Added the class that represents the DirectLine response

---
+++ src/Features/Chatbot/DirectLine/DTOs/DirectLineGetTokenDto.cs
@@ -0,0 +1,9 @@
+﻿namespace DentallApp.Features.Chatbot.DirectLine.DTOs;
+
+public class DirectLineGetTokenDto
+{
+    public string ConversationId { get; set; }
+    public string Token { get; set; }
+    [JsonProperty("expires_in")]
+    public int ExpiresIn { get; set; }
+}
