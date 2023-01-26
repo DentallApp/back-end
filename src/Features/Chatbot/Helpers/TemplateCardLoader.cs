bcb44100807ed546a43ddb953d66210c114cb508
MrDave1999 <daveseva2010@hotmail.es>
2023-01-26 12:42:34 -0500

    Added adaptive cards in the Chatbot module
    * Changed path where adaptive cards are saved
    
    * It was decided to add the templates directly in the chatbot module to have everything in one place

--- src/Features/Chatbot/Helpers/TemplateCardLoader.cs
+++ src/Features/Chatbot/Helpers/TemplateCardLoader.cs
@@ -2,20 +2,20 @@
 
 public class TemplateCardLoader
 {
-    private const string BasePath = "./TemplateCards";
+    private const string BasePath = "./Features/Chatbot/AdaptiveCards/";
 
     public static async Task<string> LoadDentalServiceCardAsync()
-        => await File.ReadAllTextAsync($"{BasePath}/DentalServiceCard.json");
+        => await File.ReadAllTextAsync($"{BasePath}DentalServiceCard.json");
 
     public static async Task<string> LoadDentistCardAsync()
-        => await File.ReadAllTextAsync($"{BasePath}/DentistCard.json");
+        => await File.ReadAllTextAsync($"{BasePath}DentistCard.json");
 
     public static async Task<string> LoadOfficeCardAsync()
-        => await File.ReadAllTextAsync($"{BasePath}/OfficeCard.json");
+        => await File.ReadAllTextAsync($"{BasePath}OfficeCard.json");
 
     public static async Task<string> LoadPatientCardAsync()
-        => await File.ReadAllTextAsync($"{BasePath}/PatientCard.json");
+        => await File.ReadAllTextAsync($"{BasePath}PatientCard.json");
 
     public static async Task<string> LoadAppointmentDateCardAsync()
-        => await File.ReadAllTextAsync($"{BasePath}/AppointmentDateCard.json");
+        => await File.ReadAllTextAsync($"{BasePath}AppointmentDateCard.json");
 }
