2e2a5e153cad9132ae4422c2b691864a30cf6891
Dave Roman <43916038+MrDave1999@users.noreply.github.com>
2023-01-28 19:20:34 -0500

    Fixed 'FormatException' exception thrown by the bot (#150)
    * Removed 'AddPrefixToUserId' from 'DirectLineAzureService'
    
    * Added a constant representing the Direct Line prefix
    
    * Added 'AddPrefixToUserId' method in abstract class
    
    * Fixed bug: Bot throws an error when the input string 'dl_1' was not in a correct format

--- src/Features/Chatbot/DirectLine/Services/DirectLineService.cs
+++ src/Features/Chatbot/DirectLine/Services/DirectLineService.cs
@@ -5,6 +5,10 @@
 /// </summary>
 public abstract class DirectLineService
 {
+    /// <summary>
+    /// A prefix <c>dl_</c> used at the beginning of the userID.
+    /// </summary>
+    public const string Prefix     = "dl_";
     public const string RequestUri = "v3/directline/tokens/generate";
     private readonly HttpClient _client;
     protected HttpClient Client => _client;
@@ -23,6 +27,14 @@ public abstract class DirectLineService
     /// <param name="namedClient">The logical name of the client to create.</param>
     public DirectLineService(IHttpClientFactory httpFactory, string namedClient)
         => _client = httpFactory.CreateClient(namedClient);
+
+    /// <summary>
+    /// Adds the prefix <c>dl_</c> to the user ID, as required by the Direct Line API.
+    /// </summary>
+    /// <param name="userId">The user ID to add the prefix.</param>
+    /// <returns>The user ID with the prefix.</returns>
+    protected static string AddPrefixToUserId(int userId)
+        => $"{Prefix}{userId}";
 
     /// <summary>
     /// Gets the Direct Line token to access a single conversation associated with the bot.
