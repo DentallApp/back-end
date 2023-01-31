c22bfdc9185e42621d07d8b6266a37f5c3156c27
MrDave1999 <daveseva2010@hotmail.es>
2023-01-31 18:32:36 -0500

    Added logic that creates instances of the user profile in a factory class
    This was added in a separate class of 'WaterfallStepContextExtensions' to facilitate unit testing.
    
    The advantage is that our new 'UserProfileFactory' class does not depend directly on 'WaterfallStepContext' (therefore it is no longer necessary to create an instance of this class in our unit tests).

---
+++ src/Features/Chatbot/Factories/UserProfileFactory.cs
@@ -0,0 +1,24 @@
+﻿namespace DentallApp.Features.Chatbot.Factories;
+
+public class UserProfileFactory
+{
+    public const string IdentifiersCouldNotBeExtractedSeparatelyMessage
+        = "The 'user ID' and 'person ID' could not be extracted separately from channel ID.\n" +
+          "This error may be caused by not following the format: " +
+          "{userID}-{personID}";
+
+    public static UserProfile Create(ChannelData channelData, string channelId)
+    {
+        var idWithoutPrefix = channelId.Replace(oldValue: DirectLineService.Prefix, newValue: string.Empty);
+        var identifiers     = idWithoutPrefix.Split("-");
+        if (identifiers.Length != 2)
+            throw new InvalidOperationException(IdentifiersCouldNotBeExtractedSeparatelyMessage);
+        var userProfile     = new UserProfile
+        {
+            UserId   = int.Parse(identifiers[0]),
+            PersonId = int.Parse(identifiers[1]),
+            FullName = channelData?.FullName
+        };
+        return userProfile;
+    }
+}
