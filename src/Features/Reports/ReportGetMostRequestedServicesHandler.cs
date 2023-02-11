f93cf74837b18c77b792638f47309c940db3b4b4
MrDave1999 <daveseva2010@hotmail.es>
2023-02-10 21:22:31 -0500

    Added new request handlers for reports
    * Created ReportGetMostRequestedServicesHandler
    
    * Created ReportGetTotalAppointmentsByDateRangeHandler
    
    * Created ReportGetTotalScheduledAppointmentsByDateRangeHandler
    
    * Used IMediator interface to send the request to the handler

---
+++ src/Features/Reports/ReportGetMostRequestedServicesHandler.cs
@@ -0,0 +1,34 @@
+﻿namespace DentallApp.Features.Reports;
+
+public class ReportGetMostRequestedServicesRequest : IRequest<IEnumerable<ReportGetMostRequestedServicesResponse>>
+{
+    public DateTime From { get; set; }
+    public DateTime To { get; set; }
+    public int OfficeId { get; set; }
+}
+
+public class ReportGetMostRequestedServicesHandler
+    : IRequestHandler<ReportGetMostRequestedServicesRequest, IEnumerable<ReportGetMostRequestedServicesResponse>>
+{
+    private readonly AppDbContext _context;
+
+    public ReportGetMostRequestedServicesHandler(AppDbContext context)
+        => _context = context;
+
+    public async Task<IEnumerable<ReportGetMostRequestedServicesResponse>> Handle(ReportGetMostRequestedServicesRequest request, CancellationToken cancellationToken)
+        => await _context.Set<Appointment>()
+                         .Include(appointment => appointment.GeneralTreatment)
+                         .Where(appointment =>
+                               (appointment.AppointmentStatusId == AppointmentStatusId.Assisted) &&
+                               (appointment.Date >= request.From && appointment.Date <= request.To))
+                         .OptionalWhere(request.OfficeId, appointment => appointment.OfficeId == request.OfficeId)
+                         .GroupBy(appointment => new { appointment.GeneralTreatmentId, appointment.GeneralTreatment.Name })
+                         .Select(group => new ReportGetMostRequestedServicesResponse
+                         {
+                             DentalServiceName = group.Key.Name,
+                             TotalAppointmentsAssisted = group.Count()
+                         })
+                         .OrderByDescending(dto => dto.TotalAppointmentsAssisted)
+                         .IgnoreQueryFilters()
+                         .ToListAsync();
+}
---
+++ src/Features/Reports/ReportGetTotalScheduledAppointmentsByDateRangeHandler.cs
@@ -0,0 +1,44 @@
+﻿namespace DentallApp.Features.Reports;
+
+public class ReportGetTotalScheduledAppointmentRequest : IRequest<IEnumerable<ReportGetTotalScheduledAppointmentResponse>>
+{
+    public DateTime From { get; set; }
+    public DateTime To { get; set; }
+    public int OfficeId { get; set; }
+}
+
+public class ReportGetTotalScheduledAppointmentsByDateRangeHandler
+    : IRequestHandler<ReportGetTotalScheduledAppointmentRequest, IEnumerable<ReportGetTotalScheduledAppointmentResponse>>
+{
+    private readonly IDbConnector _dbConnector;
+
+    public ReportGetTotalScheduledAppointmentsByDateRangeHandler(IDbConnector dbConnector)
+        => _dbConnector = dbConnector;
+
+    public async Task<IEnumerable<ReportGetTotalScheduledAppointmentResponse>> Handle(ReportGetTotalScheduledAppointmentRequest request, CancellationToken cancellationToken)
+    {
+        using var connection = _dbConnector.CreateConnection();
+        var sql = @"
+            SELECT 
+            CONCAT(p.names, ' ', p.last_names) AS DentistName,
+            o.name AS OfficeName,
+            COUNT(*) AS Total
+            FROM appointment AS a
+            INNER JOIN employee AS e ON e.id = a.dentist_id
+            INNER JOIN person AS p ON p.id = e.person_id
+            INNER JOIN office AS o ON o.id = a.office_id
+            WHERE (a.appointment_status_id = @Scheduled) AND
+	              (a.date >= @From AND a.date <= @To) AND
+	              (a.office_id = @OfficeId OR @OfficeId = 0)
+            GROUP BY a.dentist_id
+            ORDER BY Total DESC
+        ";
+        return await connection.QueryAsync<ReportGetTotalScheduledAppointmentResponse>(sql, new
+        {
+            AppointmentStatusId.Scheduled,
+            request.From,
+            request.To,
+            request.OfficeId
+        });
+    }
+}
