5d10404bfd2d55c98ed6006b0cc8bd4300a19417
MrDave1999 <daveseva2010@hotmail.es>
2023-04-02 12:33:51 -0500

    Applied the null-condition operator to format dates

--- src/Extensions/DateTimeExtensions.cs
+++ src/Extensions/DateTimeExtensions.cs
@@ -3,19 +3,14 @@
 public static class DateTimeExtensions
 {
     public static string GetDateInSpanishFormat(this DateTime dateTime)
-        => dateTime.ToString("D", new System.Globalization.CultureInfo("es-ES"));
+        => dateTime.ToString("D", new CultureInfo("es-ES"));
 
     public static string GetDateAndHourInSpanishFormat(this DateTime dateTime)
-        => dateTime.ToString("f", new System.Globalization.CultureInfo("es-ES"));
+        => dateTime.ToString("f", new CultureInfo("es-ES"));
 
     public static string GetDateWithStandardFormat(this DateTime dateTime)
         => dateTime.ToString("yyyy-MM-dd");
 
-    public static string GetDateAndHourInSpanishFormat(this DateTime? dateTimeNullable)
-    {
-        if (dateTimeNullable is null) 
-            return null;
-        DateTime dt = (DateTime)dateTimeNullable;
-        return dt.ToString("f", new System.Globalization.CultureInfo("es-ES"));
-    }
+    public static string GetDateAndHourInSpanishFormat(this DateTime? dateTime)
+        => dateTime?.ToString("f", new CultureInfo("es-ES"));
 }
