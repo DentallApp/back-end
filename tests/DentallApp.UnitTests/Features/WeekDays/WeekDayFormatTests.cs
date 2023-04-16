b4727cf97b1094468c0aeb6061ce7f03914c089c
MrDave1999 <daveseva2010@hotmail.es>
2023-04-16 11:48:23 -0500

    Applied fluent assertions in WeekDayFormatTests

--- tests/DentallApp.UnitTests/Features/WeekDays/WeekDayFormatTests.cs
+++ tests/DentallApp.UnitTests/Features/WeekDays/WeekDayFormatTests.cs
@@ -6,31 +6,38 @@ public partial class WeekDayFormatTests
     [TestMethod]
     public void GetWeekDaysFormat_WhenWeekDaysIsZero_ShouldReturnStringEmpty()
     {
+        // Arrange
         var weekDays = new List<WeekDayDto>();
         var expected = string.Empty;
 
+        // Act
         var format = WeekDayFormat.GetWeekDaysFormat(weekDays);
 
-        Assert.AreEqual(expected, actual: format);
+        // Assert
+        format.Should().Be(expected);
     }
 
     [TestMethod]
     public void GetWeekDaysFormat_WhenWeekDaysIsOne_ShouldReturnNewFormat()
     {
+        // Arrange
         var weekDays = new List<WeekDayDto>() 
         {
             new() { WeekDayId = (int)DayOfWeek.Monday, WeekDayName = WeekDaysName.Monday }
         };
         var expected = WeekDaysName.Monday;
 
+        // Act
         var format = WeekDayFormat.GetWeekDaysFormat(weekDays);
 
-        Assert.AreEqual(expected, actual: format);
+        // Assert
+        format.Should().Be(expected);
     }
 
     [TestMethod]
     public void GetWeekDaysFormat_WhenWeekDaysIsTwo_ShouldReturnNewFormat()
     {
+        // Arrange
         var weekDays = new List<WeekDayDto>()
         {
             new() { WeekDayId = (int)DayOfWeek.Monday,    WeekDayName = WeekDaysName.Monday },
@@ -38,26 +45,32 @@ public partial class WeekDayFormatTests
         };
         var expected = $"{WeekDaysName.Monday} y {WeekDaysName.Wednesday}";
 
+        // Act
         var format = WeekDayFormat.GetWeekDaysFormat(weekDays);
 
-        Assert.AreEqual(expected, actual: format);
+        // Assert
+        format.Should().Be(expected);
     }
 
     [DataTestMethod]
     [DynamicData(nameof(GetDataConsecutive), DynamicDataSourceType.Method)]
     public void GetWeekDaysFormat_WhenDaysAreConsecutive_ShouldReturnNewFormat(string testId, List<WeekDayDto> weekDays, string expected)
     {
+        // Act
         var format = WeekDayFormat.GetWeekDaysFormat(weekDays);
 
-        Assert.AreEqual(expected, actual: format);
+        // Assert
+        format.Should().Be(expected);
     }
 
     [DataTestMethod]
     [DynamicData(nameof(GetDataNotConsecutive), DynamicDataSourceType.Method)]
     public void GetWeekDaysFormat_WhenDaysAreNotConsecutive_ShouldReturnNewFormat(string testId, List<WeekDayDto> weekDays, string expected)
     {
+        // Act
         var format = WeekDayFormat.GetWeekDaysFormat(weekDays);
 
-        Assert.AreEqual(expected, actual: format);
+        // Assert
+        format.Should().Be(expected);
     }
 }
