2717a68492fcf45ddb0d731538fa1b9c47bc2f7a
MrDave1999 <daveseva2010@hotmail.es>
2023-04-17 10:48:30 -0500

    Applied fluent assertions in HeroCardFactoryTests

--- tests/DentallApp.UnitTests/Features/Chatbot/Factories/HeroCardFactoryTests.cs
+++ tests/DentallApp.UnitTests/Features/Chatbot/Factories/HeroCardFactoryTests.cs
@@ -6,6 +6,8 @@ public class HeroCardFactoryTests
     [TestMethod]
     public void CreateSchedulesHeroCard_WhenNumberOfAvailableHoursIsMultiplesOfFive_ShouldReturnHeroCardsWithAvailableHours()
     {
+        // Arrange
+        const string type = ActionTypes.MessageBack;
         var availableHours = new List<AvailableTimeRangeDto>
         {
             new() { StartHour = "07:00",  EndHour = "08:00" },
@@ -19,48 +21,44 @@ public class HeroCardFactoryTests
             new() { StartHour = "16:00",  EndHour = "16:30" },
             new() { StartHour = "17:00",  EndHour = "17:30" }
         };
-        var heroCardsExpected = new List<HeroCard>
+        var expectedHeroCards = new List<HeroCard>
         {
             new HeroCard()
             {
                 Buttons = new List<CardAction>
                 {
-                    new() { Value = "07:00 - 08:00" },
-                    new() { Value = "08:00 - 09:00" },
-                    new() { Value = "09:00 - 10:00" },
-                    new() { Value = "10:00 - 11:00" },
-                    new() { Value = "11:00 - 12:00" }
+                    new() { Title = "07:00 - 08:00", Text = "07:00 - 08:00", Value = "07:00 - 08:00", Type = type },
+                    new() { Title = "08:00 - 09:00", Text = "08:00 - 09:00", Value = "08:00 - 09:00", Type = type },
+                    new() { Title = "09:00 - 10:00", Text = "09:00 - 10:00", Value = "09:00 - 10:00", Type = type },
+                    new() { Title = "10:00 - 11:00", Text = "10:00 - 11:00", Value = "10:00 - 11:00", Type = type },
+                    new() { Title = "11:00 - 12:00", Text = "11:00 - 12:00", Value = "11:00 - 12:00", Type = type }
                 }
             },
             new HeroCard()
             {
                 Buttons = new List<CardAction>
                 {
-                    new() { Value = "12:00 - 13:00" },
-                    new() { Value = "13:00 - 14:00" },
-                    new() { Value = "15:00 - 16:00" },
-                    new() { Value = "16:00 - 16:30" },
-                    new() { Value = "17:00 - 17:30" }
+                    new() { Title = "12:00 - 13:00", Text = "12:00 - 13:00", Value = "12:00 - 13:00", Type = type },
+                    new() { Title = "13:00 - 14:00", Text = "13:00 - 14:00", Value = "13:00 - 14:00", Type = type },
+                    new() { Title = "15:00 - 16:00", Text = "15:00 - 16:00", Value = "15:00 - 16:00", Type = type },
+                    new() { Title = "16:00 - 16:30", Text = "16:00 - 16:30", Value = "16:00 - 16:30", Type = type },
+                    new() { Title = "17:00 - 17:30", Text = "17:00 - 17:30", Value = "17:00 - 17:30", Type = type }
                 }
             }
         };
 
+        // Act
         var heroCards = HeroCardFactory.CreateSchedulesHeroCard(availableHours);
 
-        Assert.AreEqual(heroCardsExpected.Count, actual: heroCards.Count);
-        for (int i = 0; i < heroCards.Count; i++)
-        {
-            var heroCard         = heroCards[i];
-            var heroCardExpected = heroCardsExpected[i];
-            Assert.AreEqual(heroCardExpected.Buttons.Count, actual: heroCard.Buttons.Count);
-            for (int j = 0; j < heroCard.Buttons.Count; j++)
-                Assert.AreEqual(heroCardExpected.Buttons[j].Value, actual: heroCard.Buttons[j].Value);
-        }
+        // Assert
+        heroCards.Should().BeEquivalentTo(expectedHeroCards);
     }
 
     [TestMethod]
     public void CreateSchedulesHeroCard_WhenNumberOfAvailableHoursIsNotMultiplesOfFive_ShouldReturnHeroCardsWithAvailableHours()
     {
+        // Arrange
+        const string type = ActionTypes.MessageBack;
         var availableHours = new List<AvailableTimeRangeDto>
         {
             new() { StartHour = "07:00",  EndHour = "08:00" },
@@ -72,67 +70,61 @@ public class HeroCardFactoryTests
             new() { StartHour = "13:00",  EndHour = "14:00" },
             new() { StartHour = "15:00",  EndHour = "16:00" }
         };
-        var heroCardsExpected = new List<HeroCard>
+        var expectedHeroCards = new List<HeroCard>
         {
             new HeroCard()
             {
                 Buttons = new List<CardAction>
                 {
-                    new() { Value = "07:00 - 08:00" },
-                    new() { Value = "08:00 - 09:00" },
-                    new() { Value = "09:00 - 10:00" },
-                    new() { Value = "10:00 - 11:00" },
-                    new() { Value = "11:00 - 12:00" }
+                    new() { Title = "07:00 - 08:00", Text = "07:00 - 08:00", Value = "07:00 - 08:00", Type = type },
+                    new() { Title = "08:00 - 09:00", Text = "08:00 - 09:00", Value = "08:00 - 09:00", Type = type },
+                    new() { Title = "09:00 - 10:00", Text = "09:00 - 10:00", Value = "09:00 - 10:00", Type = type },
+                    new() { Title = "10:00 - 11:00", Text = "10:00 - 11:00", Value = "10:00 - 11:00", Type = type },
+                    new() { Title = "11:00 - 12:00", Text = "11:00 - 12:00", Value = "11:00 - 12:00", Type = type }
                 }
             },
             new HeroCard()
             {
                 Buttons = new List<CardAction>
                 {
-                    new() { Value = "12:00 - 13:00" },
-                    new() { Value = "13:00 - 14:00" },
-                    new() { Value = "15:00 - 16:00" }
+                    new() { Title = "12:00 - 13:00", Text = "12:00 - 13:00", Value = "12:00 - 13:00", Type = type },
+                    new() { Title = "13:00 - 14:00", Text = "13:00 - 14:00", Value = "13:00 - 14:00", Type = type },
+                    new() { Title = "15:00 - 16:00", Text = "15:00 - 16:00", Value = "15:00 - 16:00", Type = type }
                 }
             }
         };
 
+        // Act
         var heroCards = HeroCardFactory.CreateSchedulesHeroCard(availableHours);
 
-        Assert.AreEqual(heroCardsExpected.Count, actual: heroCards.Count);
-        for (int i = 0; i < heroCards.Count; i++)
-        {
-            var heroCard         = heroCards[i];
-            var heroCardExpected = heroCardsExpected[i];
-            Assert.AreEqual(heroCardExpected.Buttons.Count, actual: heroCard.Buttons.Count);
-            for (int j = 0; j < heroCard.Buttons.Count; j++)
-                Assert.AreEqual(heroCardExpected.Buttons[j].Value, actual: heroCard.Buttons[j].Value);
-        }
+        // Assert
+        heroCards.Should().BeEquivalentTo(expectedHeroCards);
     }
 
     [TestMethod]
     public void CreateSchedulesHeroCard_WhenNumberOfAvailableHoursIsLessThanFive_ShouldReturnHeroCardWithAvailableHours()
     {
+        // Arrange
+        const string type = ActionTypes.MessageBack;
         var availableHours = new List<AvailableTimeRangeDto>
         {
             new() { StartHour = "07:00",  EndHour = "08:00" }
         };
-        var heroCardsExpected = new List<HeroCard>
+        var expectedHeroCards = new List<HeroCard>
         {
             new HeroCard()
             {
                 Buttons = new List<CardAction>
                 {
-                    new() { Value = "07:00 - 08:00" }
+                    new() { Title = "07:00 - 08:00", Text = "07:00 - 08:00", Value = "07:00 - 08:00", Type = type }
                 }
             }
         };
 
+        // Act
         var heroCards = HeroCardFactory.CreateSchedulesHeroCard(availableHours);
 
-        Assert.AreEqual(heroCardsExpected.Count, actual: heroCards.Count);
-        var heroCard         = heroCards[0];
-        var heroCardExpected = heroCardsExpected[0];
-        Assert.AreEqual(heroCardExpected.Buttons.Count,    actual: heroCard.Buttons.Count);
-        Assert.AreEqual(heroCardExpected.Buttons[0].Value, actual: heroCard.Buttons[0].Value);
+        // Assert
+        heroCards.Should().BeEquivalentTo(expectedHeroCards);
     }
 }
