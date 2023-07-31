5d0741a1f94e5c54abf450ae4512ca6e1ae733c0
MrDave1999 <daveseva2010@hotmail.es>
2023-07-31 11:17:23 -0500

    refactor: Added `entityId` parameter

--- src/DataAccess/AppDbContextExtensions.cs
+++ src/DataAccess/AppDbContextExtensions.cs
@@ -12,10 +12,10 @@ public static class AppDbContextExtensions
 
     public static async Task<int> SoftDeleteAsync<TEntity>(
         this AppDbContext context,
-        TEntity entityToDelete) where TEntity : SoftDeleteEntity
+        int entityId) where TEntity : SoftDeleteEntity
     {
         int updatedRows = await context.Set<TEntity>()
-                   .Where(e => e.Id == entityToDelete.Id)
+                   .Where(e => e.Id == entityId)
                    .ExecuteUpdateAsync(s => s.SetProperty(e => e.IsDeleted, true));
 
         return updatedRows;
