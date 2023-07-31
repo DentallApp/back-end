1c2f4edd80e7ff63b64651ffcb693386ee687127
MrDave1999 <daveseva2010@hotmail.es>
2023-07-31 11:04:03 -0500

    feat: Added `SoftDeleteAsync` method

--- src/DataAccess/AppDbContextExtensions.cs
+++ src/DataAccess/AppDbContextExtensions.cs
@@ -4,9 +4,20 @@ public static class AppDbContextExtensions
 {
     public static void SoftDelete<TEntity>(
         this AppDbContext context, 
-        TEntity entity) where TEntity : SoftDeleteEntity
+        TEntity entityToDelete) where TEntity : SoftDeleteEntity
     {
-        entity.IsDeleted = true;
-        context.Entry(entity).Property(e => e.IsDeleted).IsModified = true;
+        entityToDelete.IsDeleted = true;
+        context.Entry(entityToDelete).Property(e => e.IsDeleted).IsModified = true;
+    }
+
+    public static async Task<int> SoftDeleteAsync<TEntity>(
+        this AppDbContext context,
+        TEntity entityToDelete) where TEntity : SoftDeleteEntity
+    {
+        int updatedRows = await context.Set<TEntity>()
+                   .Where(e => e.Id == entityToDelete.Id)
+                   .ExecuteUpdateAsync(s => s.SetProperty(e => e.IsDeleted, true));
+
+        return updatedRows;
     }
 }
