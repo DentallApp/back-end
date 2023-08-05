94449b25f43e8592d0d7b0b0fe71d50ba02f0442
MrDave1999 <daveseva2010@hotmail.es>
2023-08-04 21:22:05 -0500

    Apply the CQRS pattern to the genders feature

---
+++ src/Features/Genders/UseCases/GetAll.cs
@@ -0,0 +1,31 @@
+﻿namespace DentallApp.Features.Genders.UseCases;
+
+public class GetGendersResponse
+{
+    public int Id { get; init; }
+    public string Name { get; init; }
+}
+
+public class GetGendersUseCase
+{
+    private readonly AppDbContext _context;
+
+    public GetGendersUseCase(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<GetGendersResponse>> Execute()
+    {
+        var genders = await _context.Set<Gender>()
+            .Select(gender => new GetGendersResponse
+            {
+                Id   = gender.Id,
+                Name = gender.Name
+            })
+            .AsNoTracking()
+            .ToListAsync();
+
+        return genders;
+    }
+}
