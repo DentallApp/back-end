e2e3ec28e38d2e37e910b077922bf29ce6ebc7d9
MrDave1999 <daveseva2010@hotmail.es>
2023-08-07 16:18:20 -0500

    Apply the CQRS pattern to the refresh token feature

---
+++ src/Features/Security/RefreshToken/UseCases/Create.cs
@@ -0,0 +1,66 @@
+﻿namespace DentallApp.Features.Security.RefreshToken.UseCases;
+
+public class CreateRefreshTokenRequest
+{
+    public string OldAccessToken { get; init; }
+    public string OldRefreshToken { get; init; }
+}
+
+public class CreateRefreshTokenResponse
+{
+    public string NewAccessToken { get; init; }
+    public string NewRefreshToken { get; init; }
+}
+
+public class CreateRefreshTokenUseCase
+{
+    private readonly AppDbContext _context;
+    private readonly ITokenService _tokenService;
+    private readonly IDateTimeProvider _dateTimeProvider;
+
+    public CreateRefreshTokenUseCase(
+        AppDbContext context, 
+        ITokenService tokenService, 
+        IDateTimeProvider dateTimeProvider)
+    {
+        _context = context;
+        _tokenService = tokenService;
+        _dateTimeProvider = dateTimeProvider;
+    }
+
+    public async Task<Response<CreateRefreshTokenResponse>> Execute(CreateRefreshTokenRequest request)
+    {
+        var claimPrincipal = _tokenService.GetPrincipalFromExpiredAccessToken(request.OldAccessToken);
+        if (claimPrincipal is null)
+            return new Response<CreateRefreshTokenResponse>(AccessTokenInvalidMessage);
+
+        int userId = claimPrincipal.GetUserId();
+        var user = await _context.Set<User>()
+            .Where(user => user.Id == userId)
+            .FirstOrDefaultAsync();
+
+        if (user is null)
+            return new Response<CreateRefreshTokenResponse>(UsernameNotFoundMessage);
+
+        if (user.RefreshToken != request.OldRefreshToken)
+            return new Response<CreateRefreshTokenResponse>(RefreshTokenInvalidMessage);
+
+        if (_dateTimeProvider.Now >= user.RefreshTokenExpiry)
+            return new Response<CreateRefreshTokenResponse>(RefreshTokenExpiredMessage);
+
+        var response = new CreateRefreshTokenResponse
+        {
+            NewAccessToken  = _tokenService.CreateAccessToken(claimPrincipal.Claims),
+            NewRefreshToken = _tokenService.CreateRefreshToken()
+        };
+        user.RefreshToken = response.NewRefreshToken;
+        await _context.SaveChangesAsync();
+
+        return new Response<CreateRefreshTokenResponse>
+        {
+            Success = true,
+            Data    = response,
+            Message = UpdatedAccessTokenMessage
+        };
+    }
+}
---
+++ src/Features/Security/RefreshToken/UseCases/Revoke.cs
@@ -0,0 +1,34 @@
+﻿namespace DentallApp.Features.Security.RefreshToken.UseCases;
+
+public class RevokeRefreshTokenUseCase
+{
+    private readonly AppDbContext _context;
+
+    public RevokeRefreshTokenUseCase(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Response> Execute(int userId)
+    {
+        var user = await _context.Set<User>()
+            .Where(user => user.Id == userId)
+            .FirstOrDefaultAsync();
+
+        if (user is null)
+            return new Response(UsernameNotFoundMessage);
+
+        if (user.RefreshToken is null)
+            return new Response(HasNoRefreshTokenMessage);
+
+        user.RefreshToken = null;
+        user.RefreshTokenExpiry = null;
+        await _context.SaveChangesAsync();
+
+        return new Response
+        {
+            Success = true,
+            Message = RevokeTokenMessage
+        };
+    }
+}
