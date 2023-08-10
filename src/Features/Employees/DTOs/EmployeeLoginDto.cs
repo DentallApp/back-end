f974e2f082cb1469081cef48c2b5d8c068c87e9e
MrDave1999 <daveseva2010@hotmail.es>
2023-08-09 20:10:12 -0500

    Deleted files

--- src/Features/Employees/DTOs/EmployeeLoginDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DentallApp.Features.Employees.DTOs;
-
-public class EmployeeLoginDto : FullEmployeeProfileDto
-{
-    public string AccessToken { get; set; }
-    public string RefreshToken { get; set; }
-}
