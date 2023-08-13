d6cb82d846093b4c11177f71ccff27d96df3cb02
MrDave1999 <daveseva2010@hotmail.es>
2023-08-13 14:16:37 -0500

    Delete files

--- src/Features/OfficeSchedules/IOfficeScheduleRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DentallApp.Features.OfficeSchedules;
-
-public interface IOfficeScheduleRepository : IRepository<OfficeSchedule>
-{
-    Task<OfficeSchedule> GetOfficeScheduleByIdAsync(int id);
-    Task<IEnumerable<OfficeScheduleGetDto>> GetOfficeScheduleByOfficeIdAsync(int officeId);
-    Task<IEnumerable<OfficeScheduleGetAllDto>> GetAllOfficeSchedulesAsync();
-    Task<IEnumerable<OfficeScheduleShowDto>> GetHomePageSchedulesAsync();
-}
