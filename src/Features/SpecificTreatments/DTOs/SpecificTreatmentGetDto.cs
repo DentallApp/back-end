65512457d31f1264731a87fcbff6a1a2e5b6a4ac
MrDave1999 <daveseva2010@hotmail.es>
2023-08-14 16:47:05 -0500

    Delete files

--- src/Features/SpecificTreatments/DTOs/SpecificTreatmentGetDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DentallApp.Features.SpecificTreatments.DTOs;
-
-public class SpecificTreatmentGetDto
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public double Price { get; set; }
-}
