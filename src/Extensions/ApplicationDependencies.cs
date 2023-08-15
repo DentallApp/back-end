064c67ebfeeae172b932eba24189c4bafc4d3d82
MrDave1999 <daveseva2010@hotmail.es>
2023-08-15 09:04:56 -0500

    Apply CQRS to employee schedules

--- src/Extensions/ApplicationDependencies.cs
+++ src/Extensions/ApplicationDependencies.cs
@@ -8,7 +8,6 @@ public static class ApplicationDependencies
                 .AddScoped<ProformaInvoiceService>()
                 .AddScoped<AppointmentService>()
                 .AddScoped<AppointmentCancellationService>()
-                .AddScoped<EmployeeScheduleService>()
                 .AddScoped<FavoriteDentistService>()
                 .AddScoped<AvailabilityService>()
                 .AddScoped<ReportDownloadPdfService>()
---
+++ src/Features/EmployeeSchedules/UseCases/Create.cs
@@ -0,0 +1,51 @@
+﻿namespace DentallApp.Features.EmployeeSchedules.UseCases;
+
+public class CreateEmployeeScheduleRequest
+{
+    public int EmployeeId { get; init; }
+    public int WeekDayId { get; init; }
+    public TimeSpan? MorningStartHour { get; init; }
+    public TimeSpan? MorningEndHour { get; init; }
+    public TimeSpan? AfternoonStartHour { get; init; }
+    public TimeSpan? AfternoonEndHour { get; init; }
+}
+
+public static class CreateEmployeeScheduleMapper
+{
+    public static EmployeeSchedule MapToEmployeeSchedule(this CreateEmployeeScheduleRequest request)
+    {
+        return new()
+        {
+            EmployeeId         = request.EmployeeId,
+            WeekDayId          = request.WeekDayId,
+            MorningStartHour   = request.MorningStartHour,
+            MorningEndHour     = request.MorningEndHour,
+            AfternoonStartHour = request.AfternoonStartHour,
+            AfternoonEndHour   = request.AfternoonEndHour
+        };
+    }
+}
+
+public class CreateEmployeeScheduleUseCase
+{
+    private readonly AppDbContext _context;
+
+    public CreateEmployeeScheduleUseCase(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Response<InsertedIdDto>> Execute(CreateEmployeeScheduleRequest request)
+    {
+        var employeeSchedule = request.MapToEmployeeSchedule();
+        _context.Add(employeeSchedule);
+        await _context.SaveChangesAsync();
+
+        return new Response<InsertedIdDto>
+        {
+            Data    = new InsertedIdDto { Id = employeeSchedule.Id },
+            Success = true,
+            Message = CreateResourceMessage
+        };
+    }
+}
