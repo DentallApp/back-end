b5aa59b5553fd5143b19bac136fb723599989063
MrDave1999 <daveseva2010@hotmail.es>
2023-08-15 08:50:18 -0500

    Delete files

--- src/Features/EmployeeSchedules/DTOs/EmployeeScheduleUpdateDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DentallApp.Features.EmployeeSchedules.DTOs;
-
-public class EmployeeScheduleUpdateDto : EmployeeScheduleDto
-{
-    public int WeekDayId { get; set; }
-    public bool IsDeleted { get; set; }
-}
