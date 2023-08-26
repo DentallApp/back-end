9208d504b3a764a397b9d9531f6a163226b5aff5
MrDave1999 <daveseva2010@hotmail.es>
2023-08-25 21:13:44 -0500

    Delete files

--- src/Features/Appointments/DTOs/AppointmentPersonDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DentallApp.Features.Appointments.DTOs;
-
-public class AppointmentPersonDto : AppointmentGetDto
-{
-    public string Document { get; set; }
-    public string CellPhone { get; set; }
-    public string Email { get; set; }
-    public DateTime? DateBirth { get; set; }
-}
