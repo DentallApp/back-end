3df975300725254f174e906dc67e49e56cdad17c
MrDave1999 <daveseva2010@hotmail.es>
2023-09-16 15:37:32 -0500

    Delete files

--- src/Features/AvailabilityHours/Models/AvailableTimeRangeRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DentallApp.Features.AvailabilityHours.Models;
-
-public class AvailableTimeRangeRequest
-{
-    public int OfficeId { get; init; }
-    public int DentistId { get; init; }
-    public int DentalServiceId { get; init; }
-    public DateTime AppointmentDate { get; init; }
-}
