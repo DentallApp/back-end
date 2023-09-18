262a86d55996e82992fe4b38b7982f8e185233a1
MrDave1999 <daveseva2010@hotmail.es>
2023-09-18 10:33:33 -0500

    Removed the `Entities` property from the generic repository

--- src/DataAccess/Repositories/Repository.cs
+++ src/DataAccess/Repositories/Repository.cs
@@ -3,31 +3,28 @@
 public class Repository<TEntity> : IRepository<TEntity> where TEntity : EntityBase
 {
     private readonly AppDbContext _context;
-    private readonly DbSet<TEntity> _entities;
 
     protected AppDbContext Context => _context;
-    protected DbSet<TEntity> Entities => _entities;
 
     public Repository(AppDbContext context)
     {
         _context = context;
-        _entities = context.Set<TEntity>();
     }
 
     public Task<TEntity> GetByIdAsync(int id)
     {
-        return _entities
+        return _context.Set<TEntity>()
             .Where(entity => entity.Id == id)
             .FirstOrDefaultAsync();
     }
 
     public void Add(TEntity entity)
-    { 
-        _entities.Add(entity);
+    {
+        _context.Add(entity);
     }
 
     public void Remove(TEntity entity)
-    { 
-        _entities.Remove(entity);
+    {
+        _context.Remove(entity);
     }
 }
