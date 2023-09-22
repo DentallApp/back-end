d77ad2189c6aba4e6146d5da297e06788941a346
MrDave1999 <daveseva2010@hotmail.es>
2023-09-22 10:22:54 -0500

    refactor: Removed methods such as AddHelpers and AddRepositories

--- src/Extensions/ApplicationDependencies.cs
+++ src/Extensions/ApplicationDependencies.cs
@@ -7,35 +7,24 @@ public static class ApplicationDependencies
     public static IServiceCollection AddServices(this IServiceCollection services)
     {
         services
-                .AddScoped(typeof(IEntityService<>), typeof(EntityService<>))
-                .AddScoped<ITokenService, TokenService>()
-                .AddScoped<IEmailService, EmailService>();
+            .AddScoped(typeof(IEntityService<>), typeof(EntityService<>))
+            .AddScoped<ITokenService, TokenService>()
+            .AddScoped<IEmailService, EmailService>()
+            .AddSingleton<IDateTimeService, DateTimeService>()
+            .AddSingleton<IHtmlConverter, HtmlConverterIText>()
+            .AddSingleton<IHtmlTemplateLoader, HtmlTemplateLoaderScriban>()
+            .AddSingleton<IPasswordHasher, PasswordHasherBcrypt>()
+            .AddSingleton<IInstantMessaging, WhatsAppMessaging>();
 
-        return services;
-    }
-
-    public static IServiceCollection AddRepositories(this IServiceCollection services)
-    {
-        services.AddScoped<IUnitOfWork, UnitOfWork>();
         services
-                .AddScoped(typeof(IRepository<>), typeof(Repository<>))
-                .AddScoped<IUserRepository, UserRepository>()
-                .AddScoped<ITreatmentRepository, TreatmentRepository>()
-                .AddScoped<IAppointmentRepository, AppointmentRepository>()
-                .AddScoped<IEmployeeScheduleRepository, EmployeeScheduleRepository>()
-                .AddScoped<ISchedulingQueries, SchedulingQueries>()
-                .AddScoped<IOfficeHolidayRepository, OfficeHolidayRepository>();
-
-        return services;
-    }
-
-    public static IServiceCollection AddHelpers(this IServiceCollection services)
-    {
-        services.AddSingleton<IHtmlConverter, HtmlConverterIText>()
-                .AddSingleton<IHtmlTemplateLoader, HtmlTemplateLoaderScriban>()
-                .AddSingleton<IPasswordHasher, PasswordHasherBcrypt>()
-                .AddSingleton<IDateTimeService, DateTimeService>()
-                .AddSingleton<IInstantMessaging, WhatsAppMessaging>();
+            .AddScoped<IUnitOfWork, UnitOfWork>()
+            .AddScoped<ISchedulingQueries, SchedulingQueries>()
+            .AddScoped(typeof(IRepository<>), typeof(Repository<>))
+            .AddScoped<IUserRepository, UserRepository>()
+            .AddScoped<ITreatmentRepository, TreatmentRepository>()
+            .AddScoped<IAppointmentRepository, AppointmentRepository>()
+            .AddScoped<IEmployeeScheduleRepository, EmployeeScheduleRepository>()
+            .AddScoped<IOfficeHolidayRepository, OfficeHolidayRepository>();
 
         return services;
     }
