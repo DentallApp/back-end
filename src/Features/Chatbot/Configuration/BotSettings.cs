bfc41b68a2d27bf313341b61f89204b80f64731b
MrDave1999 <daveseva2010@hotmail.es>
2023-10-06 11:59:20 -0500

    Delete files

--- src/Features/Chatbot/Configuration/BotSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DentallApp.Features.Chatbot.Configuration;
-
-public class BotSettings
-{
-    public string MicrosoftAppType { get; set; }
-    public string MicrosoftAppId { get; set; }
-    public string MicrosoftAppPassword { get; set; }
-    public string MicrosoftAppTenantId { get; set; }
-}
