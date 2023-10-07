7f9929d10816b3bbace8410a49fe40063d2f4417
MrDave1999 <daveseva2010@hotmail.es>
2023-10-07 12:22:54 -0500

    refactor: Added appointment reminder in a separate project
    What is the reason for the change? Well, later I will make it a plugin and it will be decoupled from ApplicationHost.

---
+++ src/AppointmentReminders/ReminderExtensions.cs
@@ -0,0 +1,31 @@
+﻿namespace DentallApp.Features.AppointmentReminders;
+
+public static class ReminderExtensions
+{
+    public static IServiceCollection AddReminderServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddQuartz(q =>
+        {
+            q.UseMicrosoftDependencyInjectionJobFactory();
+            q.AddJobAndTrigger<SendReminderJob>(configuration["REMINDER_CRON_EXPR"]);
+        });
+        services.AddQuartzHostedService(options => options.WaitForJobsToComplete = true);
+        services.AddScoped<GetScheduledAppointmentsQuery>();
+        return services;
+    }
+
+    private static IServiceCollectionQuartzConfigurator AddJobAndTrigger<TJob>(
+        this IServiceCollectionQuartzConfigurator configurator, 
+        string cronExpression) where TJob : IJob
+    {
+        var jobName = typeof(TJob).Name;
+        var jobKey  = new JobKey(jobName);
+        configurator.AddJob<TJob>(options => options.WithIdentity(jobKey));
+        configurator.AddTrigger(options => options
+            .ForJob(jobKey)
+            .WithIdentity(jobName + "-trigger")
+            .WithCronSchedule(cronExpression));
+
+        return configurator;
+    }
+}
