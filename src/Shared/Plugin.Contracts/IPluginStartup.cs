859ebb4015433f301b681d681e936a432f5e9ccc
MrDave1999 <daveseva2010@hotmail.es>
2023-11-09 12:28:02 -0500

    docs: Add XML comment to IPluginStartup

--- src/Shared/Plugin.Contracts/IPluginStartup.cs
+++ src/Shared/Plugin.Contracts/IPluginStartup.cs
@@ -1,5 +1,8 @@
 ﻿namespace DentallApp.Shared.Plugin.Contracts;
 
+/// <summary>
+/// Represents the entry point of the plugin.
+/// </summary>
 public interface IPluginStartup
 {
     void ConfigureWebApplicationBuilder(WebApplicationBuilder builder);
