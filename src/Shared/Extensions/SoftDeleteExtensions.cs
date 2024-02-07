0eab2b5a99c36743036cb979004134787b5379d9
MrDave1999 <daveseva2010@hotmail.es>
2024-02-07 17:55:05 -0500

    Create SoftDeleteExtensions.cs

---
+++ src/Shared/Extensions/SoftDeleteExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace DentallApp.Shared.Extensions;
+
+public static class SoftDeleteExtensions
+{
+    public static void SoftDelete<TEntity>(
+        this DbContext context, 
+        TEntity entityToDelete) where TEntity : SoftDeleteEntity
+    {
+        entityToDelete.IsDeleted = true;
+        context.Entry(entityToDelete).Property(e => e.IsDeleted).IsModified = true;
+    }
+
+    public static async Task<int> SoftDeleteAsync<TEntity>(
+        this DbContext context,
+        int entityId) where TEntity : SoftDeleteEntity
+    {
+        int updatedRows = await context.Set<TEntity>()
+            .Where(e => e.Id == entityId)
+            .ExecuteUpdateAsync(s => s.SetProperty(e => e.IsDeleted, true));
+
+        return updatedRows;
+    }
+}
