6b49158789e35945f84a82d958e069307b300d33
MrDave1999 <daveseva2010@hotmail.es>
2024-02-08 15:12:28 -0500

    Add fake objects to replace messaging services

---
+++ src/Infrastructure/Services/FakeInstantMessaging.cs
@@ -0,0 +1,22 @@
+﻿
+namespace DentallApp.Infrastructure.Services;
+
+public class FakeInstantMessaging(ILogger<FakeInstantMessaging> logger) : IInstantMessaging
+{
+    public string SendMessage(string phoneNumber, string message)
+    {
+        const string template =
+        """
+        PhoneNumber: {phoneNumber}
+        Message:
+        {message}
+        """;
+        logger.LogInformation(template, phoneNumber, message);
+        return string.Empty;
+    }
+
+    public Task<string> SendMessageAsync(string phoneNumber, string message)
+    {
+        return Task.FromResult(SendMessage(phoneNumber, message));
+    }
+}
