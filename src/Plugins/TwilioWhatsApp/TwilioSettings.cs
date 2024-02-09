9aee618f666418b4b85fd80e26ce45fef35f8f8f
MrDave1999 <daveseva2010@hotmail.es>
2024-02-09 15:33:58 -0500

    plugin: Add settings class for Twilio

---
+++ src/Plugins/TwilioWhatsApp/TwilioSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace DentallApp.Twilio.WhatsApp;
+
+public class TwilioSettings
+{
+    public string TwilioAccountSid { get; set; }
+    public string TwilioAuthToken { get; set; }
+    public string TwilioFromNumber { get; set; }
+    public string TwilioRegionCode { get; set; }
+}
