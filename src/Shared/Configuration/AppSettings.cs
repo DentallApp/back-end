77ab3cfbb3e75a29fa679eb86b1da0a95562be63
MrDave1999 <daveseva2010@hotmail.es>
2024-02-09 15:38:15 -0500

    cleanup: Remove properties

--- src/Shared/Configuration/AppSettings.cs
+++ src/Shared/Configuration/AppSettings.cs
@@ -5,10 +5,9 @@ namespace DentallApp.Shared.Configuration;
 /// </summary>
 public class AppSettings
 {
-    public const string MaxDaysInCalendar = "MAX_DAYS_IN_CALENDAR";
-    public const string BusinessName      = "BUSINESS_NAME";
-
+    public const string BusinessName = "BUSINESS_NAME";
     public string DentalServicesImagesPath { get; set; }
+    public string Language { get; set; }
 
     public string AccessTokenKey { get; set; }
     public double AccessTokenExpires { get; set; }
@@ -20,19 +19,4 @@ public class AppSettings
 
     public double PasswordResetTokenExpires { get; set; }
     public string PasswordResetUrl { get; set; }
-
-    [EnvKey("SENDGRID_API_KEY")]
-    public string SendGridApiKey { get; set; }
-    [EnvKey("SENDGRID_FROM_EMAIL")]
-    public string SendGridFromEmail { get; set; }
-    [EnvKey("SENDGRID_FROM_NAME")]
-    public string SendGridFromName { get; set; }
-
-    public string TwilioAccountSid { get; set; }
-    public string TwilioAuthToken { get; set; }
-    public string TwilioFromNumber { get; set; }
-    public string TwilioRegionCode { get; set; }
-
-    public int ReminderTimeInAdvance { get; set; }
-    public string ReminderCronExpr { get; set; }
 }
