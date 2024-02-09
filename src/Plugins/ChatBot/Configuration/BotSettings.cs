fd1578259b54f07c315da8c0c70dd8df67e08e41
MrDave1999 <daveseva2010@hotmail.es>
2024-02-09 15:36:52 -0500

    plugin: Add MaxDaysInCalendar property

--- src/Plugins/ChatBot/Configuration/BotSettings.cs
+++ src/Plugins/ChatBot/Configuration/BotSettings.cs
@@ -2,6 +2,7 @@
 
 public class BotSettings
 {
+    public const string MaxDaysInCalendar = "MAX_DAYS_IN_CALENDAR";
     public string MicrosoftAppType { get; set; }
     public string MicrosoftAppId { get; set; }
     public string MicrosoftAppPassword { get; set; }
--- src/Plugins/ChatBot/Factories/AdaptiveCardFactory.cs
+++ src/Plugins/ChatBot/Factories/AdaptiveCardFactory.cs
@@ -16,7 +16,7 @@ public class AdaptiveCardFactory
     public static PromptOptions CreateDateCard(string cardJson, IDateTimeService dateTimeService)
     {
         var currentDate = dateTimeService.Now.Date;
-        double maxDays = EnvReader.Instance.GetDoubleValue(AppSettings.MaxDaysInCalendar);
+        double maxDays = EnvReader.Instance.GetDoubleValue(BotSettings.MaxDaysInCalendar);
         var maxDate = currentDate.AddDays(maxDays);
         var card = AdaptiveCard.FromJson(cardJson).Card;
         var dateInput = card.Body[1] as AdaptiveDateInput;
