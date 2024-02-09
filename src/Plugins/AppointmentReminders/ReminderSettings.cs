d582e1ada1ebf40b8d1a0c3f8b7d6dee1b0efdcd
MrDave1999 <daveseva2010@hotmail.es>
2024-02-09 15:32:35 -0500

    plugin: Add settings class for reminders

---
+++ src/Plugins/AppointmentReminders/ReminderSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace DentallApp.Features.AppointmentReminders;
+
+public class ReminderSettings
+{
+    public int ReminderTimeInAdvance { get; set; }
+    public string ReminderCronExpr { get; set; }
+    public string BusinessName { get; set; }
+}
--- src/Plugins/AppointmentReminders/SendReminderJob.cs
+++ src/Plugins/AppointmentReminders/SendReminderJob.cs
@@ -2,7 +2,7 @@
 
 public class SendReminderJob(
     ILogger<SendReminderJob> logger,
-    AppSettings settings,
+    ReminderSettings settings,
     IServiceProvider serviceProvider,
     IInstantMessaging instantMessaging,
     IDateTimeService dateTimeService) : IJob
@@ -14,7 +14,7 @@ public class SendReminderJob(
         using var scope           = serviceProvider.CreateScope();
         var query                 = scope.ServiceProvider.GetRequiredService<GetScheduledAppointmentsQuery>();
         var scheduledAppointments = query.Execute(settings.ReminderTimeInAdvance, currentDateAndTime.Date);
-        var businessName          = EnvReader.Instance[AppSettings.BusinessName];
+        var businessName          = settings.BusinessName;
         foreach (var appointment in scheduledAppointments)
         {
             var message = string.Format(
