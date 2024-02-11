6450fa9990e8c313229071f2dcb9b2a7666255c8
MrDave1999 <daveseva2010@hotmail.es>
2024-02-11 12:18:43 -0500

    fix: Add TimeSpanJsonConverter type
    This fixes this error: The JSON value could not be converted to System.TimeSpan.
    This error is occurring in the use case to cancel appointments by the dentist. For example, when the client sends in the "startHour" field a value like "07:38", this error is generated.
    This error can also occur in other use cases where time slots are handled.

---
+++ src/Shared/TimeSpanJsonConverter.cs
@@ -0,0 +1,17 @@
+﻿namespace DentallApp.Shared;
+
+public class TimeSpanJsonConverter : JsonConverter<TimeSpan>
+{
+    public override TimeSpan Read(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options) =>
+            TimeSpan.Parse(reader.GetString(), CultureInfo.InvariantCulture);
+
+    public override void Write(
+        Utf8JsonWriter writer,
+        TimeSpan timeSpan,
+        JsonSerializerOptions options) =>
+            writer.WriteStringValue(timeSpan.ToString(
+                @"hh\:mm", CultureInfo.InvariantCulture));
+}
---
+++ tests/UnitTests/Shared/TimeSpanJsonConverterTests.cs
@@ -0,0 +1,66 @@
+﻿namespace UnitTests.Shared;
+
+public class TimeSpanJsonConverterTests
+{
+    class Person
+    {
+        [JsonConverter(typeof(TimeSpanJsonConverter))]
+        public TimeSpan StartHour { get; init; }
+    }
+
+    [TestCase("08:01",    8, 1, 0)]
+    [TestCase("8:1",      8, 1, 0)]
+    [TestCase("10:25",    10, 25, 0)]
+    [TestCase("08:01:25", 8, 1, 25)]
+    [TestCase("8:1:25",   8, 1, 25)]
+    public void Read_WhenDeserializingJson_ShouldConvertItToTimeSpanType(
+        string startHour,
+        int expectedHour,
+        int expectedMinute,
+        int expectedSecond)
+    {
+        // Arrange
+        var json =
+        $$"""
+        {
+            "StartHour": "{{startHour}}"
+        }
+        """;
+        var expectedTimeSpan = new TimeSpan(expectedHour, expectedMinute, expectedSecond);
+
+        // Act
+        Person person = JsonSerializer.Deserialize<Person>(json);
+        TimeSpan actual = person.StartHour;
+
+        // Assert
+        actual.Should().Be(expectedTimeSpan);
+    }
+
+    [TestCase(8, 1, 0, "08:01")]
+    [TestCase(8, 1, 10, "08:01")]
+    public void Write_WhenSerializingJson_ShouldConvertTimeSpanToHHmmFormat(
+        int hour, 
+        int minute,
+        int second,
+        string expectedStartHour)
+    {
+        // Arrange
+        var options = new JsonSerializerOptions { WriteIndented = true };
+        var person = new Person 
+        { 
+            StartHour = new TimeSpan(hour, minute, second) 
+        };
+        var expectedJson =
+        $$"""
+        {
+          "StartHour": "{{expectedStartHour}}"
+        }
+        """;
+
+        // Act
+        var jsonActual = JsonSerializer.Serialize(person, options);
+
+        // Assert
+        jsonActual.Should().Be(expectedJson);
+    }
+}
