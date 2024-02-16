404e4b092bb6c12af57e9021323425e87bae0655
MrDave1999 <daveseva2010@hotmail.es>
2024-02-16 11:34:18 -0500

    test: Add tests for an extension class

---
+++ tests/UnitTests/Shared/Entities/EmployeeSchedules/EmployeeScheduleFake.cs
@@ -0,0 +1,9 @@
+﻿namespace UnitTests.Shared.Entities.EmployeeSchedules;
+
+public class EmployeeScheduleFake : IEmployeeSchedule
+{
+    public TimeSpan? MorningStartHour { get; set; }
+    public TimeSpan? MorningEndHour { get; set; }
+    public TimeSpan? AfternoonStartHour { get; set; }
+    public TimeSpan? AfternoonEndHour { get; set; }
+}
---
+++ tests/UnitTests/Shared/Entities/EmployeeSchedules/IEmployeeScheduleExtensionsTests.cs
@@ -0,0 +1,70 @@
+﻿namespace UnitTests.Shared.Entities.EmployeeSchedules;
+
+public class IEmployeeScheduleExtensionsTests
+{
+    [Test]
+    public void IsMorningSchedule_WhenScheduleIsMorning_ShouldReturnsTrue()
+    {
+        // Arrange
+        var employeeSchedule = new EmployeeScheduleFake
+        {
+            MorningStartHour = new TimeSpan(7, 0, 0),
+            MorningEndHour   = new TimeSpan(12, 0, 0)
+        };
+
+        // Act
+        bool actual = employeeSchedule.IsMorningSchedule();
+
+        // Assert
+        actual.Should().BeTrue();
+    }
+
+    [Test]
+    public void IsAfternoonSchedule_WhenScheduleIsAfternoon_ShouldReturnsTrue()
+    {
+        // Arrange
+        var employeeSchedule = new EmployeeScheduleFake
+        {
+            AfternoonStartHour = new TimeSpan(13, 0, 0),
+            AfternoonEndHour   = new TimeSpan(18, 0, 0)
+        };
+
+        // Act
+        bool actual = employeeSchedule.IsAfternoonSchedule();
+
+        // Assert
+        actual.Should().BeTrue();
+    }
+
+    [Test]
+    public void HasNotSchedule_WhenThereAreSchedules_ShouldReturnsTrue()
+    {
+        // Arrange
+        var employeeSchedule = new EmployeeScheduleFake();
+
+        // Act
+        bool actual = employeeSchedule.HasNotSchedule();
+
+        // Assert
+        actual.Should().BeTrue();
+    }
+
+    [Test]
+    public void HasFullSchedule_WhenItHasFullSchedule_ShouldReturnsTrue()
+    {
+        // Arrange
+        var employeeSchedule = new EmployeeScheduleFake
+        {
+            MorningStartHour   = new TimeSpan(7, 0, 0),
+            MorningEndHour     = new TimeSpan(12, 0, 0),
+            AfternoonStartHour = new TimeSpan(13, 0, 0),
+            AfternoonEndHour   = new TimeSpan(18, 0, 0)
+        };
+
+        // Act
+        bool actual = employeeSchedule.HasFullSchedule();
+
+        // Assert
+        actual.Should().BeTrue();
+    }
+}
