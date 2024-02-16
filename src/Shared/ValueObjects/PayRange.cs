13b9181d0abd64e428dc1e5a726067613e2a4423
MrDave1999 <daveseva2010@hotmail.es>
2024-02-16 10:49:19 -0500

    Create directory for value objects

---
+++ src/Shared/ValueObjects/PayRange.cs
@@ -0,0 +1,20 @@
+﻿namespace DentallApp.Shared.ValueObjects;
+
+public class PayRange : ValueObject
+{
+    public double PriceMin { get; init; }
+    public double PriceMax { get; init; }
+
+    public override string ToString()
+    {
+        return PriceMin != PriceMax ?
+            new PaymentSuccess(PriceMin, PriceMax).Message :
+            new PaymentSuccess(PriceMax).Message;
+    }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return PriceMin; 
+        yield return PriceMax;
+    }
+}
