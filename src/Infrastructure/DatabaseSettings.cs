c87beced9753193d6efb2584b61efb1a70c01f8c
MrDave1999 <daveseva2010@hotmail.es>
2024-02-22 16:42:55 -0500

    Create the type representing the database configuration

---
+++ src/Infrastructure/DatabaseSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace DentallApp.Infrastructure;
+
+/// <summary>
+/// Represents the database configuration.
+/// </summary>
+public class DatabaseSettings
+{
+    [EnvKey("DB_USERNAME")]
+    public string DbUserName { get; set; } = string.Empty;
+    public string DbPassword { get; set; } = string.Empty;
+    public string DbHost { get; set; } = string.Empty;
+    public string DbDatabase { get; set; } = string.Empty;
+    public uint DbPort { get; set; }
+    public int DbMaxRetryCount { get; set; }
+    public int DbMaxRetryDelay { get; set;  }
+}
