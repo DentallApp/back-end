f72d24f08d78f741abf7b96724ed3b7204d7100c
MrDave1999 <daveseva2010@hotmail.es>
2024-02-27 15:54:41 -0500

    Create FileTypeService type

---
+++ src/Infrastructure/Services/FileTypeService.cs
@@ -0,0 +1,15 @@
+﻿namespace DentallApp.Infrastructure.Services;
+
+public class FileTypeService : IFileTypeValidator
+{
+    public Result IsImage(Stream fileContent)
+    {
+        bool isRecognizableType = FileTypeValidator.IsTypeRecognizable(fileContent);
+        if (!isRecognizableType)
+            return Result.Failure(Messages.UnrecognizableFile);
+
+        return fileContent.IsImage() ? 
+            Result.Success() : 
+            Result.Failure(Messages.IsNotImage);
+    }
+}
