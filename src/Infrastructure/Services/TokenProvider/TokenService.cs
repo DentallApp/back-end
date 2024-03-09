a2491c37f2be1e88e88eae711c9c07257115ab66
MrDave1999 <daveseva2010@hotmail.es>
2024-03-09 10:31:22 -0500

    refactor: Add private methods at the bottom of the class

--- src/Infrastructure/Services/TokenProvider/TokenService.cs
+++ src/Infrastructure/Services/TokenProvider/TokenService.cs
@@ -2,76 +2,36 @@
 
 public class TokenService(AppSettings settings, IDateTimeService dateTimeService) : ITokenService
 {
-    private string CreateJwt(IEnumerable<Claim> claims, DateTime expires, string key)
-        => JwtEncoder.Create()
-                     .WithSubject(claims)
-                     .WithExpires(expires)
-                     .WithSigningCredentials(key, SecurityAlgorithms.HmacSha256Signature)
-                     .Encode();
+    public string CreateAccessToken(UserClaims user)
+        => CreateAccessToken(CreateClaimsForUser(user));
 
-    private ClaimsPrincipal ValidateJwt(string token, string key)
-        => JwtDecoder.Create()
-                     .IgnoreValidateAudience()
-                     .IgnoreValidateIssuer()
-                     .AllowValidateIssuerSigningKey()
-                     .WithIssuerSigningKey(key)
-                     .AllowValidateLifetime()
-                     .Decode(token);
-
-    public ClaimsPrincipal ValidateEmailVerificationToken(string token)
-        => ValidateJwt(token, settings.EmailVerificationTokenKey);
-
-    public ClaimsPrincipal ValidatePasswordResetToken(string token, string passwordHash)
-        => ValidateJwt(token, key: passwordHash);
+    public string CreateAccessToken(EmployeeClaims employee)
+        => CreateAccessToken(CreateClaimsForEmployee(employee));
 
     public string CreateAccessToken(IEnumerable<Claim> claims)
-        => CreateJwt(claims, dateTimeService.UtcNow.AddMinutes(settings.AccessTokenExpires), settings.AccessTokenKey);
-
-    public string CreateAccessToken(UserClaims userClaims)
-        => CreateAccessToken(CreateClaims(userClaims));
-
-    public string CreateAccessToken(EmployeeClaims employeeClaims)
-        => CreateAccessToken(CreateClaims(employeeClaims));
+        => CreateJwt(
+            claims, 
+            expires: dateTimeService.UtcNow.AddMinutes(settings.AccessTokenExpires), 
+            key: settings.AccessTokenKey);
 
     public string CreateEmailVerificationToken(IEnumerable<Claim> claims)
-        => CreateJwt(claims, dateTimeService.UtcNow.AddHours(settings.EmailVerificationTokenExpires), settings.EmailVerificationTokenKey);
+        => CreateJwt(
+            claims, 
+            expires: dateTimeService.UtcNow.AddHours(settings.EmailVerificationTokenExpires), 
+            key: settings.EmailVerificationTokenKey);
 
     public string CreateEmailVerificationToken(UserClaims userClaims)
-        => CreateEmailVerificationToken(CreateClaims(userClaims));
+        => CreateEmailVerificationToken(CreateClaimsForUser(userClaims));
 
-    public string CreatePasswordResetToken(int userid, string username, string passwordHash)
+    public string CreatePasswordResetToken(int userId, string userName, string passwordHash)
     {
         var claims = new List<Claim>
         {
-            new (CustomClaimsType.UserId, userid.ToString()),
-            new (CustomClaimsType.UserName, username),
+            new (CustomClaimsType.UserId, userId.ToString()),
+            new (CustomClaimsType.UserName, userName),
          };
         var expires = dateTimeService.UtcNow.AddHours(settings.PasswordResetTokenExpires);
         return CreateJwt(claims, expires, key: passwordHash);
-    }
-
-    private IEnumerable<Claim> CreateClaims(UserClaims userClaims)
-    {
-        var claims = new List<Claim>
-        {
-            new (CustomClaimsType.UserId, userClaims.UserId.ToString()),
-            new (CustomClaimsType.PersonId, userClaims.PersonId.ToString()),
-            new (CustomClaimsType.UserName, userClaims.UserName),
-            new (CustomClaimsType.FullName, userClaims.FullName)
-         };
-
-        foreach (var role in userClaims.Roles)
-            claims.Add(new(ClaimTypes.Role, role));
-
-        return claims;
-    }
-
-    private IEnumerable<Claim> CreateClaims(EmployeeClaims employeeClaims)
-    {
-        var claims = CreateClaims((UserClaims)employeeClaims) as List<Claim>;
-        claims.Add(new (CustomClaimsType.EmployeeId, employeeClaims.EmployeeId.ToString()));
-        claims.Add(new (CustomClaimsType.OfficeId, employeeClaims.OfficeId.ToString()));
-        return claims;
     }
 
     public string CreateRefreshToken()
@@ -79,6 +39,12 @@ public class TokenService(AppSettings settings, IDateTimeService dateTimeService
 
     public DateTime CreateExpiryForRefreshToken()
         => dateTimeService.Now.AddDays(settings.RefreshTokenExpires);
+
+    public ClaimsPrincipal ValidateEmailVerificationToken(string token)
+        => ValidateJwt(token, settings.EmailVerificationTokenKey);
+
+    public ClaimsPrincipal ValidatePasswordResetToken(string token, string passwordHash)
+        => ValidateJwt(token, key: passwordHash);
 
     public ClaimsPrincipal GetPrincipalFromExpiredAccessToken(string token)
         => JwtDecoder.Create()
@@ -93,11 +59,51 @@ public class TokenService(AppSettings settings, IDateTimeService dateTimeService
     {
         try
         {
-            return new (new JwtSecurityToken(token).Claims);
+            return new ClaimsIdentity(new JwtSecurityToken(token).Claims);
         }
         catch(ArgumentException)
         {
             return null;
         }
+    }
+
+    private static string CreateJwt(IEnumerable<Claim> claims, DateTime expires, string key)
+        => JwtEncoder.Create()
+                     .WithSubject(claims)
+                     .WithExpires(expires)
+                     .WithSigningCredentials(key, SecurityAlgorithms.HmacSha256Signature)
+                     .Encode();
+
+    private static ClaimsPrincipal ValidateJwt(string token, string key)
+        => JwtDecoder.Create()
+                     .IgnoreValidateAudience()
+                     .IgnoreValidateIssuer()
+                     .AllowValidateIssuerSigningKey()
+                     .WithIssuerSigningKey(key)
+                     .AllowValidateLifetime()
+                     .Decode(token);
+
+    private static List<Claim> CreateClaimsForUser(UserClaims userClaims)
+    {
+        var claims = new List<Claim>
+        {
+            new (CustomClaimsType.UserId,   userClaims.UserId.ToString()),
+            new (CustomClaimsType.PersonId, userClaims.PersonId.ToString()),
+            new (CustomClaimsType.UserName, userClaims.UserName),
+            new (CustomClaimsType.FullName, userClaims.FullName)
+         };
+
+        foreach (var role in userClaims.Roles)
+            claims.Add(new(ClaimTypes.Role, role));
+
+        return claims;
+    }
+
+    private static List<Claim> CreateClaimsForEmployee(EmployeeClaims employeeClaims)
+    {
+        var claims = CreateClaimsForUser(employeeClaims);
+        claims.Add(new(CustomClaimsType.EmployeeId, employeeClaims.EmployeeId.ToString()));
+        claims.Add(new(CustomClaimsType.OfficeId,   employeeClaims.OfficeId.ToString()));
+        return claims;
     }
 }
